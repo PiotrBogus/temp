1df896e755a7fe956bfacffa6f01dd102c25ab2e
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-08-04 17:38:04 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,135 @@
-5. AI pull requests to check for IOS (from Piotr Bogus) :
-
-Add to CI/CD pipeline AI tool to check pull requests.
- Pros:
-reduces review bottlenecks
-catches more bugs early
-improve consistency 
-save time for authors and reviewers
-Cons:
-security/privacy vulnerability 
-additional tool to handle
+    [
+      {
+        "id": "internal",
+        "title": "Internal"
+      },
+      {
+        "id": "dailysales",
+        "parentId": "internal",
+        "title": "Daily Sales"
+      },
+      {
+        "id": "sales",
+        "parentId": "internal",
+        "title": "Sales"
+      },
+      {
+        "id": "globalpnl",
+        "parentId": "internal",
+        "title": "P&L Statement"
+      },
+      {
+        "id": "localpnl",
+        "parentId": "internal",
+        "title": "Region / Country P&L"
+      },
+      {
+        "id": "localmcf",
+        "parentId": "internal",
+        "title": "Region / Country MCF"
+      },
+      {
+        "id": "ytdytgsales",
+        "parentId": "internal",
+        "title": "YTD / YTG"
+      },
+      {
+        "id": "pvm",
+        "parentId": "internal",
+        "title": "PVM"
+      },
+      {
+        "id": "tfc",
+        "parentId": "internal",
+        "title": "Total Function Costs"
+      },
+      {
+        "id": "profit",
+        "parentId": "internal",
+        "title": "Profit"
+      },
+      {
+        "id": "ros",
+        "parentId": "internal",
+        "title": "Return on Sales"
+      },
+      {
+        "id": "ftepersonalcosts",
+        "parentId": "internal",
+        "title": "FTEs / Personnel Costs"
+      },
+      {
+        "id": "tacticalra",
+        "parentId": "internal",
+        "title": "Tactical RA",
+        "platform": ["web"]
+      },
+      {
+        "id": "impactanalysis",
+        "parentId": "internal",
+        "title": "Impact Analysis",
+        "platform": ["web"]
+      },
+      {
+        "id": "external",
+        "title": "External"
+      },
+      {
+        "id": "wst",
+        "parentId": "external",
+        "title": "Weekly Share Tracker"
+      },
+      {
+        "id": "mst",
+        "parentId": "external",
+        "title": "Monthly Share Tracker"
+      },
+      {
+        "id": "mtintnl",
+        "parentId": "external",
+        "title": "Market Tracker INTL Focus"
+      },
+      {
+        "id": "ptintl",
+        "parentId": "external",
+        "title": "Performance Tracker INTL"
+      },
+      {
+        "id": "ussit",
+        "parentId": "external",
+        "title": "US Stock in Trade"
+      },
+      {
+        "id": "iqvia",
+        "parentId": "external",
+        "title": "IQVIA Company Ranking"
+      },
+      {
+        "id": "ffe",
+        "parentId": "external",
+        "title": "Field Force Engagement"
+      },
+      {
+        "id": "local",
+        "title": "Local"
+      },
+      {
+        "id": "countrysales",
+        "parentId": "external",
+        "title": "Country Sales"
+      },
+      {
+        "id": "countrypnl",
+        "parentId": "external",
+        "title": "Country P&L"
+      },
+      {
+        "id": "productivity",
+        "title": "Productivity"
+      },
+      {
+        "id": "productivitykpi",
+        "parentId": "productivity",
+        "title": "Productivity KPI"
+      }
+    ]
