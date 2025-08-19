c68bc4b9e9bb4a3b345e87fac8329281e564c4cc
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-08-19 18:18:14 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,93 +1,74 @@
-import SwiftUI
+import Foundation
 import ComposableArchitecture
 
-public struct MenuView: View {
-    @Bindable var store: StoreOf<MenuFeature>
+@Reducer
+public struct MenuFeature: Sendable {
+    public init() {}
 
-    public init(store: StoreOf<MenuFeature>) {
-        self.store = store
+    @ObservableState
+    public struct State: Equatable {
+        var items: IdentifiedArrayOf<MenuItemFeature.State> = []
+
+        public init() {}
     }
 
-    public var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 12) {
-                ForEach(
-                    store.scope(
-                        state: \.items,
-                        action: \.items
-                    ),
-                    id: \.state.id
-                ) {
-                    MenuItemView(store: $0)
-                        .padding(.horizontal, 16)
-                }
-            }
-            .padding(.vertical)
-        }
-        .onAppear {
-            store.send(.onFirstAppear)
-        }
-        .background(
-            Asset.Assets.primary.swiftUIColor
-        )
+    public enum Action {
+        case delegate(Delegate)
+        case onFirstAppear
+        case didLoadMenu([MenuItemFeature.State])
+        case dismiss
+        case items(IdentifiedActionOf<MenuItemFeature>)
     }
-}
 
-struct MenuItemView: View {
-    let store: StoreOf<MenuItemFeature>
+    @Dependency(\.menuFeatureClient.log) private var log
+    @Dependency(\.menuFeatureClient.loadMenuItems) private var loadMenuItems
 
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            button
-
-            if store.isPossibleToExpand,
-               store.isExpanded {
-                childrensView
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onFirstAppear:
+                log(.debug, "menu did appear for first time")
+                return loadMenu()
+            case let .didLoadMenu(items):
+                log(.debug, "did load menu")
+                state.items = IdentifiedArrayOf(uniqueElements: items)
+                return .none
+            case .delegate:
+                return .none
+            case .dismiss:
+                return .none
+            case .items:
+                return .none
             }
         }
-        .background(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Asset.Assets.rowBlue.swiftUIColor)
-        )
-    }
-
-    private var button: some View {
-        Button(action: {
-            store.send(.didTapItem(id: store.id))
-        }, label: {
-            HStack {
-                Text(store.title)
-                    .font(.body)
-                    .foregroundStyle(Color.white)
-
-                Spacer()
-
-                if store.isPossibleToExpand {
-                    Image(systemName: store.isExpanded ? "chevron.down" : "chevron.right")
-                        .foregroundStyle(Color.white)
-                }
-            }
-            .padding(.horizontal, 16)
-        })
-        .frame(height: 48)
-    }
-
-    private var childrensView: some View {
-        VStack {
-            ForEach(
-                store.scope(
-                    state: \.childrens,
-                    action: \.childrens
-                ),
-                id: \.state.id
-            ) {
-                MenuItemView(store: $0)
-                    .padding(.horizontal, 16)
-            }
-            .padding(.leading, 16)
+        .forEach(\.items, action: \.items) {
+            MenuItemFeature()
         }
     }
 }
+
+extension MenuFeature {
+    public enum Delegate: Sendable, Equatable {}
+}
+
+private extension MenuFeature {
+    private func loadMenu() -> Effect<Action> {
+        return .run { send in
+            let groups = try? await loadMenuItems()
+            await send(.didLoadMenu(groups ?? []))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 import Foundation
@@ -109,33 +90,41 @@ public struct MenuItemFeature: Sendable {
         var isPossibleToExpand: Bool {
             childrens.isEmpty == false
         }
-
-        mutating func attachChildrens(newValue: IdentifiedArrayOf<MenuItemFeature.State>) {
-            childrens = newValue
-        }
     }
 
     public indirect enum Action {
+        case delegate(Delegate)
         case didTapItem(id: String)
         case childrens(IdentifiedActionOf<MenuItemFeature>)
     }
+
+    @Dependency(\.menuItemFeatureClient.log) private var log
 
     public var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case let .didTapItem(id):
-//                log(.debug, "did tap item: \(id)")
+                log(.debug, "did tap item: \(id)")
                 if state.isPossibleToExpand {
                     state.isExpanded = !state.isExpanded
+                } else {
+                    state.isSelected = !state.isSelected
                 }
-                return .none
+                return .send(.delegate(.didTapItem(id: id)))
             case .childrens:
                 return .none
+            case .delegate:
+                return .none
             }
+        }
+        .forEach(\.childrens, action: \.childrens) {
+            MenuItemFeature()
         }
     }
 }
 
 extension MenuItemFeature {
-    public enum Delegate: Sendable, Equatable {}
+    public enum Delegate: Sendable, Equatable {
+        case didTapItem(id: String)
+    }
 }
