3eaae05b96e417a069b85f67a074c60677715e71
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-08-20 20:29:39 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,72 +1,129 @@
-import ComposableArchitecture
 import Foundation
+import ComposableArchitecture
 
-struct TreeStructerBuilder<Result: TreeItemResult, ApiItem: ApiTreeItem> {
-    private let itemMapper: (ApiItem) -> Result
+@Reducer
+public struct MenuFeature: Sendable {
+    public init() {}
 
-    init(itemMapper: @escaping (ApiItem) -> Result) {
-        self.itemMapper = itemMapper
+    @ObservableState
+    public struct State: Equatable {
+        var items: IdentifiedArrayOf<MenuItemFeature.State> = []
+
+        public init() {}
     }
 
-    func buildTree(_ flatItems: [ApiItem]) -> [Result] {
-        // 1. Build flat lookup map
-        var lookup: [String: Result] = [:]
+    public enum Action {
+        case delegate(Delegate)
+        case onFirstAppear
+        case didLoadMenu([MenuItemFeature.State])
+        case dismiss
+        case items(IdentifiedActionOf<MenuItemFeature>)
+    }
 
-        for item in flatItems {
-            lookup[item.id] = itemMapper(item)
-        }
+    @Dependency(\.menuFeatureClient.log) private var log
+    @Dependency(\.menuFeatureClient.loadMenuItems) private var loadMenuItems
 
-        // 2. Build child relationships (child ID -> parent ID)
-        for item in flatItems {
-            guard let parentId = item.parentId else { continue }
-            guard var parent = lookup[parentId], let child = lookup[item.id] else { continue }
-
-            // Append child to the parent's children list
-            parent.childrens.append(child)
-            lookup[parentId] = parent
-        }
-
-        // 3. Recursive child population
-        func attachChildren(for node: Result) -> Result {
-            var updatedNode = node
-            updatedNode.childrens = IdentifiedArrayOf(
-                uniqueElements: node.childrens.compactMap { child in
-                    return attachChildren(for: lookup[child.id] ?? child)
-                }
-            )
-            return updatedNode
-        }
-
-
-        // 4. Collect root nodes and attach full children recursively
-        let rootNodes: [Result] = flatItems
-            .filter { $0.parentId == nil }
-            .compactMap {
-                guard let node = lookup[$0.id] else { return nil }
-                return attachChildren(for: node)
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onFirstAppear:
+                log(.debug, "menu did appear for first time")
+                return loadMenu()
+            case let .didLoadMenu(items):
+                log(.debug, "did load menu")
+                state.items = IdentifiedArrayOf(uniqueElements: items)
+                return .none
+            case .delegate:
+                return .none
+            case .dismiss:
+                return .none
+            case .items(.element(id: _, action: .delegate(.didTapItem(let id)))):
+                return .none
+            case .items:
+                return .none
             }
+        }
+        .forEach(\.items, action: \.items) {
+            MenuItemFeature()
+        }
+    }
+}
 
-        return rootNodes
+extension MenuFeature {
+    public enum Delegate: Sendable, Equatable {}
+}
+
+private extension MenuFeature {
+    private func loadMenu() -> Effect<Action> {
+        return .run { send in
+            let groups = try? await loadMenuItems()
+            await send(.didLoadMenu(groups ?? []))
+        }
     }
 }
 
 
 
-import Foundation
-
-protocol ApiTreeItem {
-    var parentId: String? { get }
-    var id: String { get }
-}
-
-
 
 import Foundation
 import ComposableArchitecture
+import Data
 
-protocol TreeItemResult: Identifiable, Equatable {
-    associatedtype Child: TreeItemResult
-    var id: String { get }
-    var parentId: String? { get }
-    var childrens: IdentifiedArrayOf<Child> { get set }
+@Reducer
+public struct MenuItemFeature: Sendable {
+    public init() {}
+
+    @ObservableState
+    public struct State: TreeItemResult {
+        public var childrens: IdentifiedArrayOf<MenuItemFeature.State> = []
+
+        public let id: String
+        let title: String
+        public let parentId: String?
+        var isExpanded: Bool = false
+        var isSelected: Bool = false
+
+        var isPossibleToExpand: Bool {
+            childrens.isEmpty == false
+        }
+    }
+
+    public indirect enum Action {
+        case delegate(Delegate)
+        case didTapItem(id: String)
+        case childrens(IdentifiedActionOf<MenuItemFeature>)
+    }
+
+    @Dependency(\.menuItemFeatureClient.log) private var log
+
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case let .didTapItem(id):
+                log(.debug, "did tap item: \(id)")
+                if state.isPossibleToExpand {
+                    state.isExpanded = !state.isExpanded
+                    return .none
+                } else {
+                    state.isSelected = !state.isSelected
+                    return .send(.delegate(.didTapItem(id: id)))
+                }
+            case .childrens(.element(id: _, action: .delegate(.didTapItem(let id)))):
+                return .send(.delegate(.didTapItem(id: id)))
+            case .childrens:
+                return .none
+            case .delegate:
+                return .none
+            }
+        }
+        .forEach(\.childrens, action: \.childrens) {
+            MenuItemFeature()
+        }
+    }
+}
+
+extension MenuItemFeature {
+    public enum Delegate: Sendable, Equatable {
+        case didTapItem(id: String)
+    }
 }
