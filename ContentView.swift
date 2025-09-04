8d0dcfa1a7d3d7190f4ea076641880b0855d1df2
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-09-04 14:29:02 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,51 +1,28 @@
-final class CarPlayCoordinator: NSObject {
-    private(set) var interfaceController: CPInterfaceController?
-    private(set) var didPop = PassthroughSubject<CPTemplate, Never>()
+    enum Action: Sendable {
+        case destination(PresentationAction<Destination.Action>)
+        case onAppear
+        case refresh
+        case template(Template)
+        case effect(Effect)
 
-    /// Rejestr: CarPlayTemplate.id → (CarPlayTemplate, CPTemplate)
-    private var registry: [UUID: (any CarPlayTemplate, CPTemplate)] = [:]
+        enum Effect: Sendable {
+            case didLoadData(CarPlayParkingsNewParkingFormModel)
+            case onValidationFormError([BuyTicketFormValidationError])
+            case onValidationFormSuccess
+            case didLoadAccounts([CarPlayParkingsAccount])
+            case didLoadSelectedCity(CarPlayParkingsCity)
+            case didLoadCars([CarPlayParkingsCarListItem])
+            case didPreauthorizeParking(CarPlayParkingsPreauthResponse)
+            case error(Error)
+        }
 
-    func save(_ interfaceController: CPInterfaceController) {
-        self.interfaceController = interfaceController
-        self.interfaceController?.delegate = self
-    }
-
-    func register(_ template: any CarPlayTemplate, cpTemplate: CPTemplate) {
-        registry[template.id] = (template, cpTemplate)
-    }
-
-    func remove(_ template: any CarPlayTemplate) {
-        registry.removeValue(forKey: template.id)
-    }
-
-    func removeAll() {
-        registry.removeAll()
-    }
-
-    func cpTemplate(for template: any CarPlayTemplate) -> CPTemplate? {
-        registry[template.id]?.1
-    }
-
-    func carPlayTemplate(for cpTemplate: CPTemplate) -> (any CarPlayTemplate)? {
-        registry.values.first(where: { $0.1 === cpTemplate })?.0
-    }
-
-    func contains(cpTemplate: CPTemplate) -> Bool {
-        interfaceController?.templates.contains(where: { $0 === cpTemplate }) ?? false
-    }
-
-    var allTemplates: [any CarPlayTemplate] {
-        registry.values.map(\.0)
-    }
-}
-
-extension CarPlayCoordinator: CPInterfaceControllerDelegate {
-    func templateDidDisappear(_ aTemplate: CPTemplate, animated: Bool) {
-        didPop.send(aTemplate)
-
-        if let carPlayTemplate = carPlayTemplate(for: aTemplate) {
-            carPlayTemplate.didDisappear(cpTemplate: aTemplate) // 🔔 callback
-            remove(carPlayTemplate)
+        enum Template: Sendable {
+            case onNextButtonTap
+            case onSelectZoneTap
+            case onSelectTimeOptionsTap
+            case onSelectAccountsTap
+            case onSelectCarTap
+            case onSelectTimeOption(CarPlayParkingsTariffTimeOption?)
+            case onErrorButtonTap(CarPlayBuyTicketFeatureErrorType)
         }
     }
-}
