7d0c699a41490fa8a92686ac1ed186124cb80b14
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-10-03 18:38:09 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,88 +1,31 @@
-//
-//  MainView.swift
-//  Genie
-//
-//  Created by Daniel Satin on 22.02.2025.
-//
-
-import ComposableArchitecture
-import KpiDailySalesPresentation
-import KpiIntlMarketTrackerPresentation
-import SwiftUI
-
-public struct MainView: View {
-    @Bindable var store: StoreOf<MainFeature>
-
-    public init(store: StoreOf<MainFeature>) {
-        self.store = store
-    }
-
-    public var body: some View {
-        ZStack {
-            content
-                .toolbar {
-                    MainBottomToolbarContent { store.send(.bottomBar($0)) }
-                }
-                .onFirstAppear { store.send(.onFirstAppear) }
-                .onAppear { store.send(.onAppear) }
-                .sheet(
-                    store: store.scope(state: \.$destination, action: \.destination)
-                ) { store in
-                    switch store.case {
-                    case let .features(store):
-                        FeaturesView(store: store)
-                    case let .notifications(store):
-                        NotificationsView(store: store)
-                    case let .profile(store):
-                        ProfileView(store: store)
-                    case let .search(store):
-                        SearchView(store: store)
+@ViewBuilder
+var menuOverlay: some View {
+    GeometryReader { geometry in
+        ZStack(alignment: .trailing) {
+            if store.menu != nil {
+                // Background dim
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            store.send(.menu(.presented(.dismiss)))
+                        }
                     }
-                }
-
-            menuOverlay
-        }
-    }
-
-    @ViewBuilder
-    var content: some View {
-        switch store.scope(state: \.content, action: \.content).case {
-        case let .dailySales(store):
-            DailySalesNavigationView(store: store)
-        case let .intlMarketTracker(store):
-            IntlMarketTrackerNavigationView(store: store)
-        }
-    }
-
-    @ViewBuilder
-    var menuOverlay: some View {
-        IfLetStore(
-            store.scope(state: \.$menu, action: \.menu),
-            then: { menuStore in
-                GeometryReader { geometry in
-                    ZStack(alignment: .trailing) {
-                        Color.black.opacity(0.5)
-                            .ignoresSafeArea()
-                            .onTapGesture {
-                                store.send(.menu(.dismiss))
-                            }
-
-                        MenuView(store: menuStore)
-                            .frame(width: geometry.size.width * 0.75,
-                                   height: geometry.size.height)
-                            .background(Color.white)
-                            .transition(.move(edge: .trailing))
-                            .shadow(radius: 8)
-                    }
-                }
-                .animation(.easeInOut, value: store.menu)
+                    .transition(.opacity)
             }
-        )
-    }
-}
 
-#Preview {
-    MainView(store: Store(initialState: MainFeature.State()) {
-        MainFeature()
-    })
+            IfLetStore(
+                store.scope(state: \.$menu, action: \.menu)
+            ) { menuStore in
+                MenuView(store: menuStore)
+                    .frame(width: geometry.size.width * 0.75,
+                           height: geometry.size.height)
+                    .background(Color.white)
+                    .shadow(radius: 8)
+                    // Push-style animation
+                    .offset(x: store.menu == nil ? geometry.size.width : 0)
+                    .animation(.easeInOut, value: store.menu != nil)
+            }
+        }
+    }
 }
