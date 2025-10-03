f00e5188d15f30e9f27b33366f4fccf186f87c02
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-10-03 15:56:00 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,149 +1,89 @@
 //
-//  MainFeature.swift
+//  MainView.swift
 //  Genie
 //
-//  Created by Daniel Satin on 17.06.2025.
+//  Created by Daniel Satin on 22.02.2025.
 //
 
 import ComposableArchitecture
 import KpiDailySalesPresentation
 import KpiIntlMarketTrackerPresentation
-import GenieCommonPresentation
+import SwiftUI
 
-@Reducer
-public struct MainFeature: Sendable {
-    public init() {}
+public struct MainView: View {
+    @Bindable var store: StoreOf<MainFeature>
 
-    @ObservableState
-    public struct State: Equatable, Sendable {
-        @Presents var destination: Destination.State?
-        @Presents var menu: MenuFeature.State?
+    public init(store: StoreOf<MainFeature>) {
+        self.store = store
+    }
 
-        var selectedMenuItemId: String?
-        @Shared(.inMemory(SharedPresentationKeys.toolbarMenuTapped)) var isToolbarMenuTapped: Bool = false
+    public var body: some View {
+        ZStack {
+            content
+                .toolbar {
+                    MainBottomToolbarContent { store.send(.bottomBar($0)) }
+                }
+                .onFirstAppear { store.send(.onFirstAppear) }
+                .onAppear { store.send(.onAppear) }
+                // --- SHEETS dla destination ---
+                .sheet(
+                    store: store.scope(state: \.$destination, action: \.destination)
+                ) { store in
+                    switch store.case {
+                    case let .features(store):
+                        FeaturesView(store: store)
+                    case let .notifications(store):
+                        NotificationsView(store: store)
+                    case let .profile(store):
+                        ProfileView(store: store)
+                    case let .search(store):
+                        SearchView(store: store)
+                    }
+                }
 
-        var content: Content.State
-        var didStartToolbarMenuTappedObservation = false
-
-        public init(
-            content: Content.State = .intlMarketTracker(.init())
-        ) {
-            self.content = content
+            // --- OVERLAY dla menu ---
+            menuOverlay
         }
     }
 
-    public enum Action: Sendable {
-        case bottomBar(BottomBarAction)
-        case content(Content.Action)
-        case destination(PresentationAction<Destination.Action>)
-        case menu(PresentationAction<MenuFeature.Action>)
-        case onAppear
-        case onFirstAppear
-        case mainMenuTapped
+    @ViewBuilder
+    var content: some View {
+        switch store.scope(state: \.content, action: \.content).case {
+        case let .dailySales(store):
+            DailySalesNavigationView(store: store)
+        case let .intlMarketTracker(store):
+            IntlMarketTrackerNavigationView(store: store)
+        }
     }
 
-    public var body: some Reducer<State, Action> {
-        Scope(state: \.content, action: \.content) {
-            Content.body
-        }
+    @ViewBuilder
+    var menuOverlay: some View {
+        if let menuStore = store.scope(state: \.$menu, action: \.menu) {
+            GeometryReader { geometry in
+                ZStack(alignment: .trailing) {
+                    // półprzezroczyste tło
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            store.send(.menu(.dismiss))
+                        }
 
-        Reduce { state, action in
-            switch action {
-            // --- Bottom bar taps
-            case .bottomBar(.featuresTapped):
-                state.destination = .features(FeaturesFeature.State())
-                return .none
-
-            case .bottomBar(.notificationsTapped):
-                state.destination = .notifications(NotificationsFeature.State())
-                return .none
-
-            case .bottomBar(.profileTapped):
-                state.destination = .profile(ProfileFeature.State())
-                return .none
-
-            case .bottomBar(.searchTapped):
-                state.destination = .search(SearchFeature.State())
-                return .none
-
-            // --- Content actions
-            case .content:
-                return .none
-
-            // --- Destination (sheets)
-            case .destination:
-                return .none
-
-            // --- Menu overlay
-            case .menu(.presented(.delegate(.dismissMenu))):
-                state.menu = nil
-                return .none
-
-            case let .menu(.presented(.delegate(.didSelectMenuItem(id)))):
-                state.selectedMenuItemId = id
-                state.menu = nil
-                return .none
-
-            case .menu:
-                return .none
-
-            // --- Lifecycle
-            case .onFirstAppear:
-                guard !state.didStartToolbarMenuTappedObservation else { return .none }
-                state.didStartToolbarMenuTappedObservation = true
-                return observeToolbarMenuTapped(state)
-
-            case .onAppear:
-                logScreen(.main)
-                return .none
-
-            // --- Open menu
-            case .mainMenuTapped:
-                state.menu = MenuFeature.State(preselectedItemId: state.selectedMenuItemId)
-                state.$isToolbarMenuTapped.withLock { $0 = false }
-                return .none
+                    // widok menu
+                    MenuView(store: menuStore)
+                        .frame(width: geometry.size.width * 0.75,
+                               height: geometry.size.height)
+                        .background(Color.white)
+                        .transition(.move(edge: .trailing))
+                        .shadow(radius: 8)
+                }
             }
+            .animation(.easeInOut, value: store.menu)
         }
-        .ifLet(\.$destination, action: \.destination)
-        .ifLet(\.$menu, action: \.menu)
     }
 }
 
-private extension MainFeature {
-    func observeToolbarMenuTapped(_ state: State) -> Effect<Action> {
-        .publisher {
-            state.$isToolbarMenuTapped.publisher
-                .removeDuplicates()
-                .filter { $0 }
-                .map { _ in .mainMenuTapped }
-                .eraseToAnyPublisher()
-        }
-        .cancellable(id: CancelID.menu, cancelInFlight: true)
-    }
+#Preview {
+    MainView(store: Store(initialState: MainFeature.State()) {
+        MainFeature()
+    })
 }
-
-extension MainFeature {
-    public enum BottomBarAction: Sendable {
-        case featuresTapped
-        case notificationsTapped
-        case profileTapped
-        case searchTapped
-    }
-
-    @Reducer(state: .equatable, .sendable, action: .sendable)
-    public enum Content {
-        case dailySales(DailySalesNavigationFeature)
-        case intlMarketTracker(IntlMarketTrackerNavigationFeature)
-    }
-
-    @Reducer(state: .sendable, .equatable, action: .sendable)
-    public enum Destination {
-        case features(FeaturesFeature)
-        case notifications(NotificationsFeature)
-        case profile(ProfileFeature)
-        case search(SearchFeature)
-        // 👈 menu usunięte stąd!
-    }
-}
-
-private enum CancelID { case menu }
