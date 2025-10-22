278e7c6de0782d6b762f71f831a08ae003dd3df2
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-10-22 14:54:27 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,241 +1,169 @@
-//
-//  MainFeature.swift
-//  Genie
-//
-//  Created by Daniel Satin on 17.06.2025.
-//
-
-import AppCompositionData
+import XCTest
 import ComposableArchitecture
-import KpiDailySalesPresentation
-import KpiIntlMarketTrackerPresentation
-import GenieCommonPresentation
+@testable import Genie
 
-@Reducer
-public struct MainFeature: Sendable {
-    public init() {}
+@MainActor
+final class MainFeatureTests: XCTestCase {
 
-    @ObservableState
-    public struct State: Equatable, Sendable {
-        @Presents var destination: Destination.State?
-        var menu: MenuFeature.State?
-        var menuItems: [MenuItemDto] = []
-        var selectedMenuItemId: Int?
+    func testOnFirstAppear_LoadsMenu_Success() async {
+        // GIVEN
+        let mockMenuItems = [
+            MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil),
+            MenuItemDto(id: 2, title: "Sales", parentId: nil, externalKPIURL: nil)
+        ]
 
-        @Shared(.inMemory(SharedPresentationKeys.toolbarMenuTapped)) var isToolbarMenuTapped: Bool = false
-        @Shared(.deviceOrientation) var deviceOrientation: DeviceOrientation = .unknown
+        let store = TestStore(initialState: MainFeature.State()) {
+            MainFeature()
+        } withDependencies: {
+            $0.mainFeatureClient.loadMenu = { mockMenuItems }
+        }
 
-        var content: Content.State
-        var didStartToolbarMenuTappedObservation = false
-        // used for animation purpose 
-        var isMenuVisible: Bool = false
-        var contentViewState: MainViewContentState = .loading
+        // WHEN
+        await store.send(.onFirstAppear)
 
-        public init(
-            content: Content.State = .intlMarketTracker(.init())
-        ) {
-            self.content = content
+        // THEN
+        await store.receive(\.didLoadMenu) {
+            $0.menuItems = mockMenuItems
+            $0.didStartToolbarMenuTappedObservation = true
+            $0.contentViewState = .content
         }
     }
 
-    public enum Action: Sendable {
-        case bottomBar(BottomBarAction)
-        case content(Content.Action)
-        case destination(PresentationAction<Destination.Action>)
-        case menu(MenuFeature.Action)
-        case onAppear
-        case onFirstAppear
-        case mainMenuTapped
-        case updateMenuVisibility(Bool)
-        case didLoadMenu([MenuItemDto])
-        case errorLoadingMenu
-        case didTapRetry
+    func testOnFirstAppear_LoadMenu_Failure() async {
+        // GIVEN
+        let store = TestStore(initialState: MainFeature.State()) {
+            MainFeature()
+        } withDependencies: {
+            $0.mainFeatureClient.loadMenu = { throw NSError(domain: "test", code: 1) }
+        }
+
+        // WHEN
+        await store.send(.onFirstAppear)
+
+        // THEN
+        await store.receive(\.errorLoadingMenu) {
+            $0.contentViewState = .error
+        }
     }
 
-    @Dependency(\.mainFeatureClient) private var mainFeatureClient
+    func testDidTapRetry_ReloadsMenu() async {
+        let mockMenuItems = [
+            MenuItemDto(id: 5, title: "Reports", parentId: nil, externalKPIURL: nil)
+        ]
 
-    public var body: some Reducer<State, Action> {
-        Scope(state: \.content, action: \.content) {
-            Content.body
+        let store = TestStore(initialState: MainFeature.State()) {
+            MainFeature()
+        } withDependencies: {
+            $0.mainFeatureClient.loadMenu = { mockMenuItems }
         }
 
-        Reduce { state, action in
-            switch action {
-            case .bottomBar(.featuresTapped):
-                state.destination = .features(FeaturesFeature.State())
-                return .none
-
-            case .bottomBar(.notificationsTapped):
-                state.destination = .notifications(NotificationsFeature.State())
-                return .none
-
-            case .bottomBar(.profileTapped):
-                state.destination = .profile(ProfileFeature.State())
-                return .none
-
-            case .bottomBar(.searchTapped):
-                state.destination = .search(SearchFeature.State())
-                return .none
-
-            case .content:
-                return .none
-
-            case .destination:
-                return .none
-
-            case .menu(.delegate(.dismissMenu)):
-                state.isMenuVisible = false
-                state.menu = nil
-                return .none
-
-            case let .menu(.delegate(.didSelectMenuItem(id))):
-                state.selectedMenuItemId = id
-                state.isMenuVisible = false
-                state.menu = nil
-                return .none
-
-            case .menu:
-                return .none
-
-            case .onFirstAppear:
-                return loadMenu()
-
-            case let .didLoadMenu(menuItems):
-                state.menuItems = menuItems
-                guard !state.didStartToolbarMenuTappedObservation else { return .none }
-                state.didStartToolbarMenuTappedObservation = true
-                state.contentViewState = .content
-                return observeToolbarMenuTapped(state)
-
-            case .errorLoadingMenu:
-                state.contentViewState = .error
-                return .none
-
-            case .didTapRetry:
-                state.contentViewState = .loading
-                return loadMenu()
-
-            case .onAppear:
-                logScreen(.main)
-                return .none
-
-            case .mainMenuTapped:
-                state.menu = MenuFeature.State(
-                    preselectedItemId: state.selectedMenuItemId,
-                    menuItems: state.menuItems
-                )
-                state.$isToolbarMenuTapped.withLock { $0 = false }
-                return .none
-            case let .updateMenuVisibility(isVisible):
-                state.isMenuVisible = isVisible
-                if !isVisible {
-                    state.menu = nil
-                }
-                return .none
-            }
+        // WHEN
+        await store.send(.didTapRetry) {
+            $0.contentViewState = .loading
         }
-        .ifLet(\.$destination, action: \.destination)
-        .ifLet(\.menu, action: \.menu) {
-            MenuFeature()
+
+        // THEN
+        await store.receive(\.didLoadMenu) {
+            $0.menuItems = mockMenuItems
+            $0.didStartToolbarMenuTappedObservation = true
+            $0.contentViewState = .content
+        }
+    }
+
+    func testMenuDelegate_DismissMenu() async {
+        var state = MainFeature.State()
+        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
+        state.isMenuVisible = true
+
+        let store = TestStore(initialState: state) {
+            MainFeature()
+        }
+
+        await store.send(.menu(.delegate(.dismissMenu))) {
+            $0.isMenuVisible = false
+            $0.menu = nil
+        }
+    }
+
+    func testMenuDelegate_DidSelectMenuItem() async {
+        var state = MainFeature.State()
+        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
+        state.isMenuVisible = true
+
+        let store = TestStore(initialState: state) {
+            MainFeature()
+        }
+
+        await store.send(.menu(.delegate(.didSelectMenuItem(99)))) {
+            $0.selectedMenuItemId = 99
+            $0.isMenuVisible = false
+            $0.menu = nil
+        }
+    }
+
+    func testUpdateMenuVisibility_False_RemovesMenu() async {
+        var state = MainFeature.State()
+        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
+        state.isMenuVisible = true
+
+        let store = TestStore(initialState: state) {
+            MainFeature()
+        }
+
+        await store.send(.updateMenuVisibility(false)) {
+            $0.isMenuVisible = false
+            $0.menu = nil
+        }
+    }
+
+    func testBottomBar_Taps_ShowDestinations() async {
+        let store = TestStore(initialState: MainFeature.State()) {
+            MainFeature()
+        }
+
+        await store.send(.bottomBar(.featuresTapped)) {
+            $0.destination = .features(FeaturesFeature.State())
+        }
+
+        await store.send(.bottomBar(.notificationsTapped)) {
+            $0.destination = .notifications(NotificationsFeature.State())
+        }
+
+        await store.send(.bottomBar(.profileTapped)) {
+            $0.destination = .profile(ProfileFeature.State())
+        }
+
+        await store.send(.bottomBar(.searchTapped)) {
+            $0.destination = .search(SearchFeature.State())
+        }
+    }
+
+    func testMainMenuTapped_ShowsMenu() async {
+        var state = MainFeature.State()
+        state.menuItems = [
+            MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil)
+        ]
+        state.selectedMenuItemId = 1
+
+        let store = TestStore(initialState: state) {
+            MainFeature()
+        }
+
+        await store.send(.mainMenuTapped) {
+            $0.menu = MenuFeature.State(
+                preselectedItemId: 1,
+                menuItems: [MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil)]
+            )
+        }
+    }
+
+    func testErrorLoadingMenu_SetsErrorState() async {
+        let store = TestStore(initialState: MainFeature.State()) {
+            MainFeature()
+        }
+
+        await store.send(.errorLoadingMenu) {
+            $0.contentViewState = .error
         }
     }
 }
-
-private extension MainFeature {
-    func loadMenu() -> Effect<Action> {
-        .run { send in
-            do {
-                let menuItems = try await mainFeatureClient.loadMenu()
-                await send(.didLoadMenu(menuItems))
-            } catch {
-                await send(.errorLoadingMenu)
-            }
-        }
-    }
-
-    func observeToolbarMenuTapped(_ state: State) -> Effect<Action> {
-        .publisher {
-            state.$isToolbarMenuTapped.publisher
-                .removeDuplicates()
-                .filter { $0 }
-                .map { _ in .mainMenuTapped }
-                .eraseToAnyPublisher()
-        }
-        .cancellable(id: CancelID.menu, cancelInFlight: true)
-    }
-}
-
-extension MainFeature {
-    public enum BottomBarAction: Sendable {
-        case featuresTapped
-        case notificationsTapped
-        case profileTapped
-        case searchTapped
-    }
-
-    @Reducer(state: .equatable, .sendable, action: .sendable)
-    public enum Content {
-        case dailySales(DailySalesNavigationFeature)
-        case intlMarketTracker(IntlMarketTrackerNavigationFeature)
-    }
-
-    @Reducer(state: .sendable, .equatable, action: .sendable)
-    public enum Destination {
-        case features(FeaturesFeature)
-        case notifications(NotificationsFeature)
-        case profile(ProfileFeature)
-        case search(SearchFeature)
-    }
-}
-
-private enum CancelID { case menu }
-
-
-
-
-
-
-
-
-
-
-//
-//  MainFeatureClient.swift
-//  Genie
-//
-//  Created by Daniel Satin on 27.06.2025.
-//
-
-import Combine
-import AppCompositionData
-import Dependencies
-import DependenciesMacros
-import Network
-
-@DependencyClient
-struct MainFeatureClient: DependencyKey {
-    var networkStatus: @Sendable () async -> AnyPublisher<(isOnline: Bool, status: NWPath.Status), Never> = {
-        Just((true, .satisfied))
-            .eraseToAnyPublisher()
-    }
-    var loadMenu: @Sendable () async throws -> [MenuItemDto]
-
-    static let liveValue = MainFeatureClient(
-        networkStatus: {
-            @Dependency(\.networkSignalRepository) var repository
-            return await repository.networkStatusPublisher()
-        },
-        loadMenu: {
-            @Dependency(\.menuRepository) var menuRepository
-            return try await menuRepository.loadMenu()
-        }
-    )
-}
-
-extension DependencyValues {
-    var mainFeatureClient: MainFeatureClient {
-        get { self[MainFeatureClient.self] }
-        set { self[MainFeatureClient.self] = newValue }
-    }
-}
-
