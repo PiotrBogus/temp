1ae3364cbb78c514923af727aafc86712c84c05a
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-10-23 20:27:52 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,169 +1,54 @@
-import XCTest
-import ComposableArchitecture
-@testable import Genie
+import CarPlay
+import Foundation
 
-@MainActor
-final class MainFeatureTests: XCTestCase {
-
-    func testOnFirstAppear_LoadsMenu_Success() async {
-        // GIVEN
-        let mockMenuItems = [
-            MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil),
-            MenuItemDto(id: 2, title: "Sales", parentId: nil, externalKPIURL: nil)
-        ]
-
-        let store = TestStore(initialState: MainFeature.State()) {
-            MainFeature()
-        } withDependencies: {
-            $0.mainFeatureClient.loadMenu = { mockMenuItems }
-        }
-
-        // WHEN
-        await store.send(.onFirstAppear)
-
-        // THEN
-        await store.receive(\.didLoadMenu) {
-            $0.menuItems = mockMenuItems
-            $0.didStartToolbarMenuTappedObservation = true
-            $0.contentViewState = .content
+extension CPInterfaceController {
+    func dismissAndPresent(
+        template: CPTemplate,
+        animated: Bool = true,
+        completion: ((Bool, (any Error)?) -> Void)? = nil
+    ) {
+        if presentedTemplate != nil {
+            dismissTemplate(animated: false) { [weak self] _, _ in
+                DispatchQueue.main.async {
+                    self?.presentTemplate(template, animated: animated, completion: completion)
+                }
+            }
+        } else {
+            presentTemplate(template, animated: animated, completion: completion)
         }
     }
 
-    func testOnFirstAppear_LoadMenu_Failure() async {
-        // GIVEN
-        let store = TestStore(initialState: MainFeature.State()) {
-            MainFeature()
-        } withDependencies: {
-            $0.mainFeatureClient.loadMenu = { throw NSError(domain: "test", code: 1) }
-        }
-
-        // WHEN
-        await store.send(.onFirstAppear)
-
-        // THEN
-        await store.receive(\.errorLoadingMenu) {
-            $0.contentViewState = .error
-        }
-    }
-
-    func testDidTapRetry_ReloadsMenu() async {
-        let mockMenuItems = [
-            MenuItemDto(id: 5, title: "Reports", parentId: nil, externalKPIURL: nil)
-        ]
-
-        let store = TestStore(initialState: MainFeature.State()) {
-            MainFeature()
-        } withDependencies: {
-            $0.mainFeatureClient.loadMenu = { mockMenuItems }
-        }
-
-        // WHEN
-        await store.send(.didTapRetry) {
-            $0.contentViewState = .loading
-        }
-
-        // THEN
-        await store.receive(\.didLoadMenu) {
-            $0.menuItems = mockMenuItems
-            $0.didStartToolbarMenuTappedObservation = true
-            $0.contentViewState = .content
+    func dismissAndPush(
+        template: CPTemplate,
+        animated: Bool = true,
+        completion: ((Bool, (any Error)?) -> Void)? = nil
+    ) {
+        if presentedTemplate != nil {
+            dismissTemplate(animated: false) { [weak self] _, _ in
+                DispatchQueue.main.async {
+                    self?.pushTemplate(template, animated: animated, completion: completion)
+                }
+            }
+        } else {
+            pushTemplate(template, animated: animated, completion: completion)
         }
     }
 
-    func testMenuDelegate_DismissMenu() async {
-        var state = MainFeature.State()
-        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
-        state.isMenuVisible = true
-
-        let store = TestStore(initialState: state) {
-            MainFeature()
-        }
-
-        await store.send(.menu(.delegate(.dismissMenu))) {
-            $0.isMenuVisible = false
-            $0.menu = nil
-        }
-    }
-
-    func testMenuDelegate_DidSelectMenuItem() async {
-        var state = MainFeature.State()
-        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
-        state.isMenuVisible = true
-
-        let store = TestStore(initialState: state) {
-            MainFeature()
-        }
-
-        await store.send(.menu(.delegate(.didSelectMenuItem(99)))) {
-            $0.selectedMenuItemId = 99
-            $0.isMenuVisible = false
-            $0.menu = nil
-        }
-    }
-
-    func testUpdateMenuVisibility_False_RemovesMenu() async {
-        var state = MainFeature.State()
-        state.menu = MenuFeature.State(preselectedItemId: nil, menuItems: [])
-        state.isMenuVisible = true
-
-        let store = TestStore(initialState: state) {
-            MainFeature()
-        }
-
-        await store.send(.updateMenuVisibility(false)) {
-            $0.isMenuVisible = false
-            $0.menu = nil
-        }
-    }
-
-    func testBottomBar_Taps_ShowDestinations() async {
-        let store = TestStore(initialState: MainFeature.State()) {
-            MainFeature()
-        }
-
-        await store.send(.bottomBar(.featuresTapped)) {
-            $0.destination = .features(FeaturesFeature.State())
-        }
-
-        await store.send(.bottomBar(.notificationsTapped)) {
-            $0.destination = .notifications(NotificationsFeature.State())
-        }
-
-        await store.send(.bottomBar(.profileTapped)) {
-            $0.destination = .profile(ProfileFeature.State())
-        }
-
-        await store.send(.bottomBar(.searchTapped)) {
-            $0.destination = .search(SearchFeature.State())
-        }
-    }
-
-    func testMainMenuTapped_ShowsMenu() async {
-        var state = MainFeature.State()
-        state.menuItems = [
-            MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil)
-        ]
-        state.selectedMenuItemId = 1
-
-        let store = TestStore(initialState: state) {
-            MainFeature()
-        }
-
-        await store.send(.mainMenuTapped) {
-            $0.menu = MenuFeature.State(
-                preselectedItemId: 1,
-                menuItems: [MenuItemDto(id: 1, title: "Home", parentId: nil, externalKPIURL: nil)]
-            )
-        }
-    }
-
-    func testErrorLoadingMenu_SetsErrorState() async {
-        let store = TestStore(initialState: MainFeature.State()) {
-            MainFeature()
-        }
-
-        await store.send(.errorLoadingMenu) {
-            $0.contentViewState = .error
+    func dismissAndSetAsRoot(
+        template: CPTemplate,
+        animated: Bool = true,
+        completion: ((Bool, (any Error)?) -> Void)? = nil
+    ) {
+        if presentedTemplate != nil {
+            dismissTemplate(animated: false) { [weak self] _, _ in
+                DispatchQueue.main.async {
+                    self?.setRootTemplate(template, animated: animated, completion: completion)
+                }
+            }
+        } else {
+            setRootTemplate(template, animated: animated, completion: completion)
         }
     }
 }
+
+extension CPInterfaceController: @retroactive @unchecked Sendable {}
