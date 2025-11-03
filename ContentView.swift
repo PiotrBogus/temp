897dbdabad6ad812904f96fa0733cb2fe0e3517d
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-11-03 21:02:54 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,114 +1,258 @@
-import XCTest
-import ComposableArchitecture
-import AppCompositionDomain
-@testable import AppCompositionPresentation
+import  ComposableArchitecture
 
-@MainActor
-final class MenuFeatureTests: XCTestCase {
-    func testOnFirstAppear_LoadsAppInfo() async {
-        let mockMenuItems = [
-            MenuItemFeature.State(children: [], id: 1, parentId: nil, title: "Home"),
-            MenuItemFeature.State(children: [], id: 2, parentId: nil, title: "Settings")
+@Reducer
+struct BehavioralBiometricExplanationReducer: Sendable {
+    @ObservableState
+    struct State: Equatable, Sendable {
+        @Presents var destination: Destination.State?
+
+        let questionsAndAnswersItems: [BehavioralBiometricQuestionAndAnswerItem] = [
+            .init(
+                title: "InfoScreen_btn_Question01",
+                description: "InfoScreen_lbl_Answer01"
+            ),
+            .init(
+                title: "InfoScreen_btn_Question02",
+                description: "InfoScreen_lbl_Answer02"
+            ),
+            .init(
+                title: "InfoScreen_btn_Question03",
+                description: "InfoScreen_lbl_Answer03"
+            ),
+            .init(
+                title: "InfoScreen_btn_Question04",
+                description: "InfoScreen_lbl_Answer04"
+            ),
+            .init(
+                title: "InfoScreen_btn_Question05",
+                description: "InfoScreen_lbl_Answer05"
+            )
         ]
 
-        let store = TestStore(
-            initialState: MenuFeature.State(menuItems: mockMenuItems)
-        ) {
-            MenuFeature()
-        } withDependencies: {
-            $0.menuFeatureClient.log = { _, _ in }
-            $0.menuFeatureClient.appInfoViewModel = {
-                AppInfoViewModel(version: "1.0", build: "100", environment: "Dev")
+        var expandedId: UUID?
+    }
+
+    @Reducer(state: .equatable, .sendable, action: .sendable)
+    public enum Destination {
+        case faq(BehavioralBiometricFAQReducer)
+    }
+
+    enum Action: Sendable {
+        case onTurnOnAdditionalSecurityButtonTap
+        case onShowAllQuestionsAndAnswersButtonTap
+        case onQuestionTap(UUID?)
+        case destination(PresentationAction<Destination.Action>)
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onTurnOnAdditionalSecurityButtonTap:
+                return .none
+            case .onShowAllQuestionsAndAnswersButtonTap:
+                state.destination = .faq(.init())
+                return .none
+            case let .onQuestionTap(id):
+                state.expandedId = id
+                return .none
+            case .destination:
+                return .none
             }
         }
-
-        await store.send(.onFirstAppear) {
-            $0.appVersion = "1.0"
-            $0.appBuild = "100"
-            $0.appEnvironment = "Dev"
-        }
-    }
-
-    func testDismiss_SendsDelegate() async {
-        let store = TestStore(
-            initialState: MenuFeature.State(menuItems: [])
-        ) {
-            MenuFeature()
-        } withDependencies: {
-            $0.menuFeatureClient.log = { _, _ in }
-            $0.menuFeatureClient.appInfoViewModel = { nil }
-        }
-
-        await store.send(.dismiss)
-        await store.receive(\.delegate.dismissMenu)
-    }
-
-    func testSelectMenuItem_TriggersDelegate() async {
-        let item = MenuItemFeature.State(
-            children: [],
-            id: 1,
-            parentId: nil,
-            title: "Dashboard",
-            isExpanded: false,
-            isSelected: false
-        )
-
-        let store = TestStore(
-            initialState: MenuFeature.State(menuItems: [item])
-        ) {
-            MenuFeature()
-        } withDependencies: {
-            $0.menuFeatureClient.log = { _, _ in }
-            $0.menuFeatureClient.appInfoViewModel = { nil }
-        }
-
-        await store.send(
-            .items(
-                .element(
-                    id: item.id,
-                    action: .delegate(.didTapItem(.init(id: item.id, isExpand: false)))
-                )
-            )
-        ) {
-            $0.items[id: item.id]?.isSelected = true
-        }
-
-        await store.receive(\.delegate.didSelectMenuItem) {
-            // Można tu zweryfikować, że item jest wybrany
-            $0.items[id: 1]?.isSelected = true
-        }
-    }
-
-    func testExpandMenuItem_KeepsExpandedState() async {
-        var expandableItem = MenuItemFeature.State(
-            children: [
-                MenuItemFeature.State(children: [], id: 2, parentId: 1, title: "Child")
-            ],
-            id: 1,
-            parentId: nil,
-            title: "Parent",
-            isExpanded: false,
-            isSelected: false
-        )
-        expandableItem.identifiedArrayOfChildrens = IdentifiedArrayOf(uniqueElements: expandableItem.children)
-
-        let store = TestStore(
-            initialState: MenuFeature.State(menuItems: [expandableItem])
-        ) {
-            MenuFeature()
-        } withDependencies: {
-            $0.menuFeatureClient.log = { _, _ in }
-            $0.menuFeatureClient.appInfoViewModel = { nil }
-        }
-
-        await store.send(
-            .items(
-                .element(
-                    id: 1,
-                    action: .delegate(.didTapItem(.init(id: 1, isExpand: true)))
-                )
-            )
-        )
-        // brak receive — akcja expand nie emituje efektu, tylko modyfikuje stan
+        .ifLet(\.$destination, action: \.destination)
     }
 }
+
+
+
+
+import Assets
+import ComposableArchitecture
+import DesignSystemSwiftUI
+import SwiftUI
+
+struct BehavioralBiometricExplanationView: View {
+    private struct Constants {
+        static let commonPadding: CGFloat = 16
+        static let imageSize: CGFloat = 96
+        static let imageVerticalPadding: CGFloat = 24
+        static let statusLabelFontStyle: UIFont.Style = .regular(.size12LS16, color: .ikoWhite100)
+        static let statusLabelPadding: CGFloat = 4
+        static let statusLabelCornerRadius: CGFloat = 4
+        static let statusLabelTopPadding: CGFloat = 12
+        static let headerTitleLabelFontStyle: UIFont.Style = .regular(.size24LS32)
+        static let headerDescriptionFontStyle: UIFont.Style = .regular(.size18LS22)
+        static let headerDescriptionLabelTopPadding: CGFloat = 14
+        static let itemImageSize: CGFloat = 48
+        static let itemHeaderFontStyle: UIFont.Style = .bold(.size16LS22)
+        static let headerTopPadding: CGFloat = 20
+        static let itemSpacing: CGFloat = 10
+        static let answerFontStyle: UIFont.Style = .regular(.size16LS22)
+        static let showAllQuestionsAndAnswersButtonFontStyle:  UIFont.Style = .regular(.size14LS18, color: .ikoBlue100)
+    }
+
+    var store: StoreOf<BehavioralBiometricExplanationReducer>
+
+    init(store: StoreOf<BehavioralBiometricExplanationReducer>) {
+        self.store = store
+    }
+
+    var body: some View {
+        ZStack {
+            VStack {
+                ScrollView() {
+                    VStack(spacing: Constants.itemSpacing) {
+                        headerView
+                        howItWorksSection
+                        questionsAndAnswersSection
+                    }
+                }
+                bottomPrimaryButtonView
+            }
+        }
+        .background(Color(uiColor: .ikoBlue0))
+        .navigationDestination(
+            store: store.scope(state: \.$destination, action: .destination)
+        ) {
+
+        }
+    }
+
+    private var headerView: some View {
+        ZStack {
+            VStack(spacing: .zero) {
+                Image(uiImage: Assets.imageNamed(IKOImages.ILU_ESG_SOCIETY)!)
+                    .resizable()
+                    .frame(width: Constants.imageSize, height: Constants.imageSize)
+                    .padding(.top, Constants.imageVerticalPadding)
+
+                VStack {
+                    AttributedText("Darmowa usługa")
+                        .fontStyle(Constants.statusLabelFontStyle)
+                        .padding(Constants.statusLabelPadding)
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: Constants.statusLabelCornerRadius).fill(Color(uiColor: .ikoGreen100))
+                )
+                .padding(.top, Constants.statusLabelTopPadding)
+
+                AttributedText("Dadtkowe zabezpieczenie")
+                    .fontStyle(Constants.headerTitleLabelFontStyle)
+                    .padding(.top, Constants.headerTopPadding)
+
+                AttributedText("Wzmocnij ochronę Twoich pieniędzy przed oszustami")
+                    .fontStyle(Constants.headerDescriptionFontStyle)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, Constants.headerTopPadding)
+                    .padding(.bottom, Constants.imageVerticalPadding)
+                    .padding(.horizontal, Constants.commonPadding)
+            }
+        }
+        .background(Color(uiColor: .ikoWhite100))
+    }
+
+    private var howItWorksSection: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            HeaderView(text:"Jak to działa?")
+
+            VStack(alignment: .leading, spacing: Constants.itemSpacing) {
+                createItemHeader(
+                    image: Assets.imageNamed(IKOImages.PKO_ICON_MORE_PROTECTION)!,
+                    title: "Wzmacnia bezpieczeństwo Twoich pieniędzy"
+                )
+                AttributedText(
+                    "Sprawdzamy, czy to na pewno Ty logujesz się do aplikacji IKO."
+                )
+            }
+            .padding(.horizontal, Constants.commonPadding)
+
+            VStack(alignment: .leading, spacing: Constants.itemSpacing) {
+                createItemHeader(
+                    image: Assets.imageNamed(IKOImages.PKO_ICON_ADVISER_INDIVIDUAL)!,
+                    title: "Tworzy profil Twoich zachować"
+                )
+                AttributedText(
+                    "Rozwiązanie analizuje Twoje cechy zachowania m.in. tempo i sposób pisania na klawiaturze lub używanie ekranu dotykowego.."
+                )
+            }
+            .padding(.horizontal, Constants.commonPadding)
+
+            VStack(alignment: .leading, spacing: .zero) {
+                createItemHeader(
+                    image: Assets.imageNamed(IKOImages.PKO_ICON_DESKTOP_MOBILE)!,
+                    title: "Analizuje parametry urządzenia z którego logujesz się do aplikacji IKO"
+                )
+                ContentMessageView(
+                    icon: .hint,
+                    title: "Dodatkowe zabezpieczenie nie zbiera informacji o:",
+                    subtitle: "* Twoim loginie i haśle,\n* danych kontaktowych,\n* saldach, produktach i transakcjach"
+                )
+                .padding(.vertical, Constants.commonPadding)
+            }
+            .padding(.horizontal, Constants.commonPadding)
+        }
+        .background(Color(uiColor: .ikoWhite100))
+    }
+
+    private func createItemHeader(image: UIImage, title: String) -> some View {
+        HStack(spacing: Constants.commonPadding) {
+            Image(uiImage: image)
+                .frame(
+                    width: Constants.itemImageSize,
+                    height: Constants.itemImageSize
+                )
+            AttributedText(title)
+                .fontStyle(Constants.itemHeaderFontStyle)
+        }
+        .padding(.top, Constants.headerTopPadding)
+    }
+
+    private var questionsAndAnswersSection: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            Separator()
+            HStack {
+                HeaderView(text: "Pytania i odpowiedzi")
+                    .with(topSeparatorHidden: true)
+                    .with(bottomSeparatorHidden: true)
+                Spacer()
+                Button {
+                    store.send(.onShowAllQuestionsAndAnswersButtonTap)
+                } label: {
+                    AttributedText("Wszystkie")
+                        .fontStyle(Constants.showAllQuestionsAndAnswersButtonFontStyle)
+                }
+                .padding(.trailing, Constants.commonPadding)
+            }
+            ForEach(store.questionsAndAnswersItems) { item in
+                let binding = Binding(
+                    get: { store.expandedId == item.id },
+                    set: { isExpanded in
+                        store.send(.onQuestionTap(isExpanded ? item.id : nil))
+                    }
+                )
+                Collapse(isExpanded: binding) {
+                    AttributedText(item.description)
+                        .fontStyle(Constants.answerFontStyle)
+                } header: {
+                    HeaderCollapse(title: item.title)
+                        .headerStyle(.default(hideBottomSeparatorOnCollapsed: true))
+                }
+            }
+        }
+        .background(Color(uiColor: .ikoWhite100))
+    }
+
+    private var bottomPrimaryButtonView: some View {
+        VStack {
+            PrimaryButton(title: "Włącz dodatkowe zabezpieczenie") {
+                store.send(.onTurnOnAdditionalSecurityButtonTap)
+            }
+            .padding(Constants.commonPadding)
+        }
+        .background(Color(uiColor: .ikoWhite100))
+    }
+}
+
+//#Preview {
+//    BehavioralBiometricExplanationView()
+//}
