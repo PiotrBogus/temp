4afdc12625173fc582cf45d296d6ce59e6bc37c4
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-11-10 20:00:29 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,26 +1,17 @@
-struct TableItem: Identifiable, Equatable, Sendable {
-    let id = UUID()
-    let title: String
-    let color: Color
-
-    static let mock: [TableItem] = [
-        .init(title: "Novartis Brand", color: .black),
-        .init(title: "6,7%", color: .black),
-        .init(title: "16ppt", color: .green),
-        .init(title: "1,7ppt", color: .red),
-    ]
-}
-
-struct TableRow: Identifiable, Equatable, Sendable {
+struct TableColumn: Identifiable, Equatable, Sendable {
     let id = UUID()
     let items: [TableItem]
+}
 
-    static let mock: [TableRow] = [
-        .init(items: TableItem.mock),
-        .init(items: TableItem.mock),
-        .init(items: TableItem.mock),
-        .init(items: TableItem.mock),
-        .init(items: TableItem.mock),
-        .init(items: TableItem.mock)
-    ]
+
+extension Array where Element == TableRow {
+    func transposed() -> [TableColumn] {
+        guard let firstRow = self.first else { return [] }
+        let columnCount = firstRow.items.count
+
+        return (0..<columnCount).map { columnIndex in
+            let columnItems = self.map { $0.items[columnIndex] }
+            return TableColumn(items: columnItems)
+        }
+    }
 }
