97484ee0311a80acd42dd8d97ff53bf992e5e819
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-11-11 18:53:49 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -18,7 +18,32 @@ actor FontWidthCacheActorImplementation {
         init(value: CGFloat) {
             self.value = value
             self.accessCount = 1
-            self.lastAccessed = Date()
+            self.lastAccessed = Date()import SwiftUI
+import UIKit
+
+private static func measureWidth(string: String, font: Font) -> CGFloat {
+    // Spróbuj zamienić SwiftUI.Font na UIFont
+    let uiFont: UIFont
+    switch font {
+    case .largeTitle: uiFont = .preferredFont(forTextStyle: .largeTitle)
+    case .title: uiFont = .preferredFont(forTextStyle: .title1)
+    case .headline: uiFont = .preferredFont(forTextStyle: .headline)
+    case .body: uiFont = .preferredFont(forTextStyle: .body)
+    default: uiFont = .systemFont(ofSize: UIFont.systemFontSize)
+    }
+
+    let attributes: [NSAttributedString.Key: Any] = [.font: uiFont]
+    let size = (string as NSString).boundingRect(
+        with: CGSize(width: CGFloat.greatestFiniteMagnitude,
+                     height: CGFloat.greatestFiniteMagnitude),
+        options: [.usesLineFragmentOrigin, .usesFontLeading],
+        attributes: attributes,
+        context: nil
+    ).size
+
+    return ceil(size.width)
+}
+
         }
         
         mutating func accessed() {
