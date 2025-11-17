95a1bea608030f87929afd2e8bfea7c69b8d38ae
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-11-17 18:08:43 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,30 +1,69 @@
 import Assembly
 import Dependencies
 import DependenciesMacros
-import IKOCommon
-import SwinjectAutoregistration
+import UIKit
+import UIComponents
 
-@DependencyClient
-struct BehavioralBiometricAgreementsReducerClient: DependencyKey, Sendable {
-    var loadAgreements: @Sendable () async throws -> [BehavioralBiometricAgreement]
-    var enableBehavioralBiometric: @Sendable (IKOUIPin) async throws -> Void
+struct BehavioralBiometricErrorViewControllerModel: Sendable {
+    let viewController: UIViewController
+    let header: String
+    let title: String
+    let okButtonTitle: String
+    let okButtonBehex: IKOBehexEventId
+    let okButtonHandler: @Sendable () -> Void
+    let tryAgainButtonTitle: String
+    let tryAgainButtonBehex: IKOBehexEventId
+    let tryAgainButtonHandler: @Sendable () -> Void
+    let viewShowBehex: IKOBehexEventId
+}
 
-    static let liveValue: BehavioralBiometricAgreementsReducerClient = {
-        let networkService: BehavioralBiometricNetworkServiceProviding = IKOAssembler.resolver~>
+struct BehavioralBiometricErrorNavigationProvider: DependencyKey {
+    var presentErrorViewController: @MainActor @Sendable (BehavioralBiometricErrorViewControllerModel) -> Void
 
-        return BehavioralBiometricAgreementsReducerClient(
-            loadAgreements: {
-                try await networkService.getBehavioralBiometricAgreements()
-            },
-            enableBehavioralBiometric { mPin in
-                try await networkService.changeBehavioralBiometricStatus(isEnabled: true, mPin: mPin)
+    static var liveValue: BehavioralBiometricErrorNavigationProvider = {
+        BehavioralBiometricErrorNavigationProvider(
+            presentErrorViewController: { model in
+                guard let contentView = IKOResultContentView(
+                    assetName: IKO_ANIMATION_ICON_RESULT_ERROR,
+                    title: model.title,
+                    subtitle: nil,
+                    token: nil,
+                    resultType: .IKOConfirmationResultError
+                ) else { return }
+
+                let bottomView = IKOBlueAndWhiteButtonBottomView(
+                    blueButtonTitle: model.tryAgainButtonTitle,
+                    whiteButtonTitle: model.okButtonTitle
+                )
+                bottomView?.whiteButton?.iko_addEventHandler({ _ in
+                    IKOAssembler.resolveBehex()?.register(event: model.okButtonBehex)
+                    model.okButtonHandler()
+                }, forControlEvents: .primaryActionTriggered)
+                bottomView?.blueButton?.iko_addEventHandler({ _ in
+                    IKOAssembler.resolveBehex()?.register(event: model.tryAgainButtonBehex)
+                    model.tryAgainButtonHandler()
+                }, forControlEvents: .primaryActionTriggered)
+
+                guard let controller = IKOResultViewController(
+                    contentView: contentView,
+                    bottomView: bottomView,
+                    cancelButtonResultHandler: { _ in
+                        IKOAssembler.resolveBehex()?.register(event: model.okButtonBehex
+                        )
+                        model.okButtonHandler()
+                    }) else { return }
+
+                controller.resultNavigationBarTitle = model.header
+                controller.behexEventId = model.viewShowBehex
+                model.viewController.present(controller, animated: true)
             }
         )
     }()
 }
+
 extension DependencyValues {
-    var behavioralBiometricAgreementsReducerClient: BehavioralBiometricAgreementsReducerClient {
-        get { self[BehavioralBiometricAgreementsReducerClient.self] }
-        set { self[BehavioralBiometricAgreementsReducerClient.self] = newValue }
+    var behavioralBiometricErrorNavigationProvider: BehavioralBiometricErrorNavigationProvider {
+        get { self[BehavioralBiometricErrorNavigationProvider.self] }
+        set { self[BehavioralBiometricErrorNavigationProvider.self] = newValue }
     }
 }
