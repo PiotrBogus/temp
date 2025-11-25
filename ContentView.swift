373207a7c90a5e98f97c4fb0431b14892d718ec5
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-11-25 12:23:21 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,245 +1,234 @@
-#import "IKOBlueAndWhiteButtonBottomView.h"
-#import "UIView+LabelConfiguration.h"
-#import <DesignSystemTokens/DesignSystemTokens-Swift.h>
-#import <DesignSystemUIKit/DesignSystemUIKit-Swift.h>
-#import <UIComponents/UIComponents-Swift.h>
+Select a definition
 
-@implementation IKOBlueAndWhiteButtonBottomView
+Genie.NextApi
+Genie.NextGen.Api
+ 1.0 
+OAS 3.0
+https://ci-api-next.dev.genie.novartis.net/swagger/../swagger/v1/swagger.json
 
-@synthesize additionalContentView = _additionalContentView;
+Authorize
+Common
 
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle
-                       whiteButtonTitle:(NSString *)whiteButtonTitle {
-    return [self initWithBlueButtonTitle:blueButtonTitle whiteButtonTitle:whiteButtonTitle secondWhiteButtonTitle:nil hint:nil];
+FeatureFlags
+
+FinanceWatchlist
+
+MarketTracker
+
+
+POST
+/mst/v1/data
+Provides data part of KPI, this part is loaded always when selectors are changed
+
+
+
+POST
+/mst/v1/tableData
+Returns the table data for the Market Tracker KPI. This endpoint is called whenever selectors are changed.
+
+
+Parameters
+Cancel
+No parameters
+
+Request body
+
+application/json
+The query model containing the list of selected filters.
+
+Edit Value
+Schema
+{
+  "selectors": [
+    {
+      "selectorName": "string",
+      "selectorValue": "string"
+    }
+  ]
 }
+Execute
+Clear
+Responses
+Curl
 
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle andSeparator:(BOOL)showSeparator {
-    return [self initWithBlueButtonTitle:blueButtonTitle whiteButtonTitle:whiteButtonTitle secondWhiteButtonTitle:nil hint:nil hintConfiguration:nil andSeparator:showSeparator];
-}
+curl -X 'POST' \
+  'https://ci-api-next.dev.genie.novartis.net/mst/v1/tableData' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "selectors": [
+    {
+      "selectorName": "string",
+      "selectorValue": "string"
+    }
+  ]
+}'
+Request URL
+https://ci-api-next.dev.genie.novartis.net/mst/v1/tableData
+Server response
+Code	Details
+401
+Undocumented
+Error: response status is 401
 
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle
-                       whiteButtonTitle:(NSString *)whiteButtonTitle
-                                   hint:(NSString *)hint {
-    return [self initWithBlueButtonTitle:blueButtonTitle whiteButtonTitle:whiteButtonTitle secondWhiteButtonTitle:nil hint:hint];
+Response headers
+ access-control-allow-origin: * 
+ content-length: 0 
+ date: Tue,25 Nov 2025 11:21:49 GMT 
+ strict-transport-security: max-age=15724800; includeSubDomains 
+ www-authenticate: Bearer,Bearer 
+Responses
+Code	Description	Links
+200	
+OK
 
-}
+Media type
 
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle
-                       whiteButtonTitle:(NSString *)whiteButtonTitle
-                 secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle {
-    return [self initWithBlueButtonTitle:blueButtonTitle whiteButtonTitle:whiteButtonTitle secondWhiteButtonTitle:secondWhiteButtonTitle hint:nil hintConfiguration:nil andSeparator:NO];
-}
-
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle hint:(NSString *)hint {
-    return [self initWithBlueButtonTitle:blueButtonTitle whiteButtonTitle:whiteButtonTitle secondWhiteButtonTitle:secondWhiteButtonTitle hint:hint hintConfiguration:[self smallLightGrayConfiguration] andSeparator:NO];
-}
-
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle
-                       whiteButtonTitle:(NSString *)whiteButtonTitle
-                 secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle
-                                   hint:(NSString *)hint
-                      hintConfiguration:(void (^)(Label *))configuration
-                           andSeparator:(BOOL)showSeparator {
-    self = [super initWithFrame:[UIScreen mainScreen].bounds];
-    if (self) {
-        self.translatesAutoresizingMaskIntoConstraints = NO;
-        _useBottomSafeAreaGuide = YES;
-
-        if (hint) {
-            _hintLabel = [[[[Label fontWeight:lightWeight size:size16LS24] alignment:NSTextAlignmentCenter] multiline] textColor:ikoGray140];
-
-            if (configuration) {
-                configuration(_hintLabel);
+application/json
+Controls Accept header.
+Example Value
+Schema
+{
+  "title": "string",
+  "selectors": [
+    {
+      "id": 0,
+      "name": "string",
+      "title": "string",
+      "defaultValue": "string",
+      "resetSelectors": true,
+      "type": 0,
+      "allowMultiselect": true,
+      "synchronizationGroupId": 0,
+      "options": [
+        {
+          "id": "string",
+          "title": "string",
+          "subtitle": "string",
+          "isEnabled": true
+        }
+      ],
+      "maxMultiselectValues": 0
+    }
+  ],
+  "columnSelectors": [
+    {
+      "id": 0,
+      "title": "string",
+      "type": 0,
+      "allowMultiselect": true,
+      "maxSelectedColumns": 0,
+      "minSelectedColumns": 0,
+      "defaultColumnsIds": [
+        0
+      ],
+      "options": [
+        0
+      ]
+    }
+  ],
+  "columns": [
+    {
+      "id": 0,
+      "title": "string",
+      "subtitle": "string",
+      "sortable": true,
+      "valueAlignment": 0,
+      "titleAlignment": 0,
+      "multiValueHandling": {
+        "handling": 0,
+        "togglingGroupId": 0
+      },
+      "coloring": 0
+    }
+  ],
+  "layouts": [
+    {
+      "type": {
+        "platform": 0,
+        "orientation": 0
+      },
+      "visibleColumnGroups": [
+        {
+          "id": 0,
+          "title": "string",
+          "associatedSelectorIds": [
+            0
+          ]
+        }
+      ],
+      "visibleColumns": [
+        {
+          "toggleColumnIds": [
+            0
+          ],
+          "defaultColumnId": 0,
+          "measurePosition": 0,
+          "hasRightBorder": true,
+          "columnGroupId": 0,
+          "associatedSelectors": [
+            {
+              "selectorId": 0,
+              "selectorType": 0
             }
-
-            _hintLabel.text = hint;
-            [self addSubview:_hintLabel];
+          ]
         }
-
-        if (secondWhiteButtonTitle) {
-            _secondWhiteButton = [IKOButton buttonWithLegacyType:IKOButtonLegacyTypeBigWhite];
-            [_secondWhiteButton setTitle:secondWhiteButtonTitle forState:UIControlStateNormal];
-            [self addSubview:_secondWhiteButton];
-        }
-
-        _blueButton = [IKOButton buttonWithLegacyType:IKOButtonLegacyTypeBigBlue];
-        [_blueButton setTitle:blueButtonTitle forState:UIControlStateNormal];
-        [self addSubview:_blueButton];
-
-        _whiteButton = [IKOButton buttonWithLegacyType:IKOButtonLegacyTypeBigWhite];
-        [_whiteButton setTitle:whiteButtonTitle forState:UIControlStateNormal];
-        _whiteButton.hidden = whiteButtonTitle == nil;
-        [self addSubview:_whiteButton];
-
-        _order = IKOBlueAndWhiteButtonOrderWhiteFirst;
-
-        if (showSeparator) {
-            self.backgroundColor = UIColor.ikoWhite100;
-            _separator = [UIView ikoSeparatorView];
-            [self addSubview:_separator];
-        }
+      ],
+      "selectorsRowContent": [
+        0
+      ]
     }
-
-    return self;
-}
-
-- (void)updateConstraints {
-    CGFloat horizontalMargin = IKOViewSizeMediumMargin;
-    CGFloat topMargin = IKOViewSizeMediumMargin;
-
-    UIView *firstView;
-    UIView *secondView;
-    UIView *secondWhiteButtonRightButtonView;
-
-    if(self.separator){
-        [self.separator mas_remakeConstraints:^(MASConstraintMaker *make) {
-            make.left.right.top.equalTo(self);
-            make.height.equalTo(@(IKOViewSizeSeparatorDefaultHeight));
-        }];
+  ],
+  "dataFormats": [
+    {
+      "id": 0,
+      "unitType": 0,
+      "unitOfMeasure": "string",
+      "digits": 0,
+      "dateFormat": "string",
+      "isBold": true,
+      "isItalic": true
     }
-
-    if (_order == IKOBlueAndWhiteButtonOrderBlueFirst || _order == IKOBlueAndWhiteButtonOrderHorizontalBlueFirst) {
-        firstView = _blueButton;
-        secondView = _whiteButton;
-        secondWhiteButtonRightButtonView = secondView;
-    } else if (_order == IKOBlueAndWhiteButtonOrderWhiteFirst || _order == IKOBlueAndWhiteButtonOrderHorizontalWhiteFirst) {
-        firstView = _whiteButton;
-        secondView = _blueButton;
-        secondWhiteButtonRightButtonView = firstView;
-    }
-
-    if (self.additionalContentView) {
-        [self.additionalContentView mas_remakeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self).offset(topMargin);
-            make.left.equalTo(self).offset(horizontalMargin);
-            make.right.equalTo(self).offset(-horizontalMargin);
-        }];
-    }
-
-    [self.hintLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
-        if (self.additionalContentView) {
-            make.top.equalTo(self.additionalContentView.mas_bottom).offset(topMargin);
-        } else {
-            make.top.equalTo(self).offset(topMargin);
-        }
-        make.left.equalTo(self).offset(horizontalMargin);
-        make.right.equalTo(self).offset(-horizontalMargin);
-    }];
-
-    [firstView mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.mas_left).offset(horizontalMargin);
-        if ([self isHorizontalOrder]) {
-            make.right.equalTo(self.mas_centerX).offset(-horizontalMargin / 2.f);
-            make.height.equalTo(secondView);
-        } else if (!(_secondWhiteButton && _order == IKOBlueAndWhiteButtonOrderWhiteFirst)) {
-            make.right.equalTo(self.mas_right).offset(-horizontalMargin);
-            make.height.equalTo(@(kBlueButtonHeight));
-        }
-
-        if (self.hintLabel) {
-            make.top.equalTo(self.hintLabel.mas_bottom).offset(topMargin);
-        } else {
-            if (self.additionalContentView) {
-                make.top.equalTo(self.additionalContentView.mas_bottom).offset(topMargin);
-            } else {
-                make.top.equalTo(self.mas_top).offset(topMargin);
+  ],
+  "rows": [
+    {
+      "id": 0,
+      "hasChart": true,
+      "hasDrillDown": true,
+      "rowType": 0,
+      "drillDownInfo": {
+        "drillDownId": 0,
+        "drillDownParamType": "string",
+        "drillDownTitle": "string"
+      },
+      "cells": [
+        {
+          "columnId": 0,
+          "values": [
+            {
+              "valueId": 0,
+              "value": "string",
+              "dataFormatId": 0,
+              "coloring": 0
             }
+          ]
         }
-    }];
-
-    [self.secondWhiteButton mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self).offset(-horizontalMargin);
-        make.top.bottom.equalTo(secondWhiteButtonRightButtonView);
-        make.left.equalTo(secondWhiteButtonRightButtonView.mas_right).offset(horizontalMargin);
-        make.width.equalTo(secondWhiteButtonRightButtonView.mas_width);
-    }];
-
-    [secondView mas_updateConstraints:^(MASConstraintMaker *make) {
-        if ([self isHorizontalOrder]) {
-            make.top.equalTo(firstView);
-            make.left.equalTo(self.mas_centerX).offset(horizontalMargin / 2.f);
-            make.height.equalTo(firstView);
-        } else {
-            make.left.equalTo(self.mas_left).offset(horizontalMargin);
-            make.top.equalTo(firstView.mas_bottom).offset(topMargin);
-            make.height.equalTo(@(kBlueButtonHeight));
-        }
-        make.right.equalTo(self.mas_right).offset(-horizontalMargin);
-        if (_useBottomSafeAreaGuide) {
-            make.bottom.equalTo(self.mas_safeAreaLayoutGuideBottom).offset(-topMargin);
-        } else {
-            make.bottom.equalTo(self.mas_bottom).offset(-topMargin);
-        }
-    }];
-
-    [super updateConstraints];
-}
-
-- (BOOL)isHorizontalOrder {
-    return _order == IKOBlueAndWhiteButtonOrderHorizontalBlueFirst || _order == IKOBlueAndWhiteButtonOrderHorizontalWhiteFirst;
-}
-
-- (void)setAdditionalContentView:(UIView *)additionalContentView {
-    if (!additionalContentView) {
-        [_additionalContentView removeFromSuperview];
-        _additionalContentView = nil;
-        [self setNeedsUpdateConstraints];
-        return;
+      ],
+      "parentId": 0,
+      "groupId": 0
     }
-
-    _additionalContentView = [UIView new];
-    _additionalContentView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self addSubview:self.additionalContentView];
-    [self.additionalContentView addSubview:additionalContentView];
-    [additionalContentView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self.additionalContentView);
-    }];
-
-    [self setNeedsUpdateConstraints];
+  ]
 }
+No links
+410	
+Endpoint version no longer supported
 
-- (void)lock {
-    self.blueButton.enabled = NO;
-}
+No links
 
-- (void)unlock {
-    self.blueButton.enabled = YES;
-}
-
-@end
-
+POST
+/mst/v1/chart
+Provides Chart part of KPI, this part is loaded always when selectors are changed
 
 
 
-
-
-
-
-
-
-
-#import "IKOAdditionalContentView.h"
-#import "IKOBaseView.h"
-#import "IKOLockableView.h"
-#import <Foundation/Foundation.h>
-
-@class IKOButton;
-@class Label;
-
-@interface IKOBlueAndWhiteButtonBottomView : IKOBaseView <IKOAdditionalContentView, IKOLockableView>
-
-@property(nonatomic, strong) IKOButton *whiteButton;
-@property(nonatomic, strong) IKOButton *secondWhiteButton;
-@property(nonatomic, strong) IKOButton *blueButton;
-@property(nonatomic, strong) Label *hintLabel;
-@property(nonatomic, assign) IKOBlueAndWhiteButtonOrder order;
-@property(nonatomic, strong) UIView *separator;
-@property(nonatomic) BOOL useBottomSafeAreaGuide;
-
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle NS_SWIFT_NAME(init(blueButtonTitle:whiteButtonTitle:));
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle andSeparator:(BOOL)showSeparator NS_SWIFT_NAME(init(blueButtonTitle:whiteButtonTitle:showSeparator:));
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle hint:(NSString *)hint;
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle;
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle hint:(NSString *)hint;
-- (instancetype)initWithBlueButtonTitle:(NSString *)blueButtonTitle whiteButtonTitle:(NSString *)whiteButtonTitle secondWhiteButtonTitle:(NSString *)secondWhiteButtonTitle hint:(NSString *)hint hintConfiguration:(void (^)(Label *))configuration andSeparator:(BOOL)showSeparator;
-@end
+Schemas
