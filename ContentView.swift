ef827868f253aa778009eff31714fca55d3f90f7
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-12-05 12:04:26 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,70 +1,8 @@
-struct MenuItemView: View {
-    @Bindable var store: StoreOf<MenuItemFeature>
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 0) {   // spacing 0 → zero „szpar”
-            
-            button
-
-            if store.isPossibleToExpand,
-               store.isExpanded {
-                childrenView
-            }
-        }
-        .background(Color.darkBlueBackground) // JEDYNE źródło tła
-        .task { store.send(.onAppear) }
-    }
-
-    private var button: some View {
-        Button {
-            store.send(.didTapItem(id: store.id))
-        } label: {
-            HStack {
-                Text(store.title)
-                    .font(.title3)
-                    .fontWeight(store.isExpanded || store.isSelected ? .semibold : .regular)
-                    .foregroundColor(createTextColor(store.isSelected))
-
-                Spacer()
-
-                if store.isPossibleToExpand {
-                    Image(systemName: store.isExpanded ? "chevron.down" : "chevron.right")
-                        .foregroundColor(.white)
-                }
-            }
-            .padding(.horizontal, 16)
-            .frame(height: 52)
-            .background(
-                RoundedRectangle(cornerRadius: 26)
-                    .fill(createRowBackgroundColor())
-            )
+    private func handleFeatureContition() -> Bool {
+        withCheckedContinuation { continuation in
+            let networkServiceProvider: BehavioralBiometricNetworkServiceProvider = IKOAssembler.resolver~>
+            let state = try? await networkServiceProvider.getBehavioralBiometricState()
+            let isDisabled = state?.enabled == false
+            continuation.resume(returning: isDisabled)
         }
     }
-
-    private var childrenView: some View {
-        VStack(spacing: 0) {      // zero spacing = brak białych „cieni”
-            ForEach(
-                store.scope(state: \.identifiedArrayOfChildrens, action: \.children),
-                id: \.state.id
-            ) {
-                MenuItemView(store: $0)
-            }
-            separatorView
-        }
-    }
-
-    var separatorView: some View {
-        Rectangle()
-            .fill(Color.white)
-            .frame(height: 1)
-            .padding(.horizontal, 4)
-    }
-
-    private func createTextColor(_ isSelected: Bool) -> Color {
-        isSelected ? .darkBlueBackground : .whiteText
-    }
-
-    private func createRowBackgroundColor() -> Color {
-        store.isSelected ? .whiteBackground : .darkBlueBackground
-    }
-}
