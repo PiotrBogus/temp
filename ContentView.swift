cbd6c928c833dc6fc5fd113306528fe2482c5a7e
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-12-09 17:29:12 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,180 +1,279 @@
+import XCTest
 import ComposableArchitecture
-import UIKit
-import CoreTelephony
-import Foundation
+@testable import YourAppModule
 
-@Reducer
-struct ESimActivationReducer {
-    @ObservableState
-    struct State: Sendable {
-        var destination: Destination?
-        let data: ESimActivationData
-        var isLoading = true
-        var isSystemConfiguratorAvailable = false
-        var currentNumberOfInstalledSims: Int = 0
+@MainActor
+final class ESimActivationReducerTests: XCTestCase {
+    
+    // MARK: - Test Data
+    
+    private func makeTestData() -> ESimActivationData {
+        ESimActivationData(
+            lpa: "test-lpa",
+            smdpAddress: "test-smdp",
+            activationCode: "test-code",
+            iosActivationUrl: "https://test.esim.com/activate",
+            confirmationCode: "123456",
+            carrierName: "Test Carrier",
+            planLabel: "Test Plan",
+            cardNumber: "1234567890"
+        )
     }
-
-    @CasePathable
-    enum Action: Sendable {
-        case onAppear
-        case onNumberSimInstalled(Int)
-        case onActivationSuccess
-        case onManualESimAdd
-        case onOpenSystemConfigurator
-        case onESimNotSupported
-        case onCopyTap(String)
-        case onDeviceSettingTap
-        case onAppDidBecomeActive
+    
+    private func makeTestState() -> ESimActivationReducer.State {
+        ESimActivationReducer.State(
+            data: makeTestData(),
+            isLoading: true,
+            isSystemConfiguratorAvailable: false,
+            currentNumberOfInstalledSims: 0
+        )
     }
-
-    @CasePathable
-    enum Destination: Sendable {
-        case esimNotSupported
-        case activationSuccess
-        case eSimPhoneSettings
-        case activation(String)
-    }
-
-    let eSimManager: ESimProvisioningManaging
-
-    init(eSimManager: ESimProvisioningManaging) {
-        self.eSimManager = eSimManager
-    }
-
-    var body: some ReducerOf<Self> {
-        Reduce { state, action in
-            switch action {
-            case .onAppear:
-                return updateNumberOfInstalledSims()
-
-            case let .onNumberSimInstalled(simsCount):
-                state.currentNumberOfInstalledSims = simsCount
-                return checkESimAvailability(configuratorLink: state.data.iosActivationUrl)
-
-            case .onESimNotSupported:
-                state.destination = .esimNotSupported
-                return .none
-
-            case .onActivationSuccess:
-                state.destination = .activationSuccess
-                return .none
-
-            case .onManualESimAdd:
-                state.isLoading = false
-                return .none
-
-            case .onOpenSystemConfigurator:
-                state.isSystemConfiguratorAvailable = true
-                state.isLoading = false
-                state.destination = .activation(state.data.iosActivationUrl)
-                return .none
-
-            case let .onCopyTap(text):
-                UIPasteboard.general.string = text
-                return .none
-
-            case .onDeviceSettingTap:
-                if state.isSystemConfiguratorAvailable {
-                    state.destination = .activation(state.data.iosActivationUrl)
-                } else {
-                    state.destination = .eSimPhoneSettings
-                }
-                return .none
-
-            case .onAppDidBecomeActive:
-                if state.currentNumberOfInstalledSims < eSimManager.numberOfAvailableSims() {
-                    state.destination = .activationSuccess
-                }
-                return .none
-            }
+    
+    // MARK: - onAppear Tests
+    
+    func testOnAppear_UpdatesNumberOfInstalledSims() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockNumberOfSims = 2
+        
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onAppear)
+        await store.receive(.onNumberSimInstalled(2)) {
+            $0.currentNumberOfInstalledSims = 2
         }
     }
-
-    func checkESimAvailability(configuratorLink: String) -> Effect<Action> {
-        .run { send in
-            if await eSimManager.isESimSupportedByDevice() {
-                if await eSimManager.isSystemESimConfiguratorAvailable(configuratorLink: configuratorLink) {
-                    await send(.onOpenSystemConfigurator)
-                } else {
-                    await send(.onManualESimAdd)
-                }
-            } else {
-                await send(.onESimNotSupported)
-            }
+    
+    // MARK: - onNumberSimInstalled Tests
+    
+    func testOnNumberSimInstalled_WithSystemConfigurator_OpensSystemConfigurator() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockIsESimSupported = true
+        mockManager.mockIsConfiguratorAvailable = true
+        
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onNumberSimInstalled(1)) {
+            $0.currentNumberOfInstalledSims = 1
+        }
+        await store.receive(.onOpenSystemConfigurator) {
+            $0.isSystemConfiguratorAvailable = true
+            $0.isLoading = false
+            $0.destination = .activation("https://test.esim.com/activate")
         }
     }
-
-    func updateNumberOfInstalledSims() -> Effect<Action> {
-        .run { send in
-            let simsCount = eSimManager.numberOfAvailableSims()
-            await send(.onNumberSimInstalled(simsCount))
+    
+    func testOnNumberSimInstalled_WithoutSystemConfigurator_TriggersManualAdd() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockIsESimSupported = true
+        mockManager.mockIsConfiguratorAvailable = false
+        
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onNumberSimInstalled(1)) {
+            $0.currentNumberOfInstalledSims = 1
         }
+        await store.receive(.onManualESimAdd) {
+            $0.isLoading = false
+        }
+    }
+    
+    func testOnNumberSimInstalled_ESimNotSupported_ShowsNotSupportedDestination() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockIsESimSupported = false
+        
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onNumberSimInstalled(0)) {
+            $0.currentNumberOfInstalledSims = 0
+        }
+        await store.receive(.onESimNotSupported) {
+            $0.destination = .esimNotSupported
+        }
+    }
+    
+    // MARK: - onESimNotSupported Tests
+    
+    func testOnESimNotSupported_SetsDestination() async {
+        let mockManager = MockESimProvisioningManager()
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onESimNotSupported) {
+            $0.destination = .esimNotSupported
+        }
+    }
+    
+    // MARK: - onActivationSuccess Tests
+    
+    func testOnActivationSuccess_SetsDestination() async {
+        let mockManager = MockESimProvisioningManager()
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onActivationSuccess) {
+            $0.destination = .activationSuccess
+        }
+    }
+    
+    // MARK: - onManualESimAdd Tests
+    
+    func testOnManualESimAdd_StopsLoading() async {
+        let mockManager = MockESimProvisioningManager()
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onManualESimAdd) {
+            $0.isLoading = false
+        }
+    }
+    
+    // MARK: - onOpenSystemConfigurator Tests
+    
+    func testOnOpenSystemConfigurator_UpdatesStateAndDestination() async {
+        let mockManager = MockESimProvisioningManager()
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onOpenSystemConfigurator) {
+            $0.isSystemConfiguratorAvailable = true
+            $0.isLoading = false
+            $0.destination = .activation("https://test.esim.com/activate")
+        }
+    }
+    
+    // MARK: - onCopyTap Tests
+    
+    func testOnCopyTap_CopiesTextToPasteboard() async {
+        let mockManager = MockESimProvisioningManager()
+        let store = TestStore(
+            initialState: makeTestState(),
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        let testText = "test-activation-code"
+        await store.send(.onCopyTap(testText))
+        
+        XCTAssertEqual(UIPasteboard.general.string, testText)
+    }
+    
+    // MARK: - onDeviceSettingTap Tests
+    
+    func testOnDeviceSettingTap_WithSystemConfigurator_OpensActivation() async {
+        let mockManager = MockESimProvisioningManager()
+        var state = makeTestState()
+        state.isSystemConfiguratorAvailable = true
+        
+        let store = TestStore(
+            initialState: state,
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onDeviceSettingTap) {
+            $0.destination = .activation("https://test.esim.com/activate")
+        }
+    }
+    
+    func testOnDeviceSettingTap_WithoutSystemConfigurator_OpensPhoneSettings() async {
+        let mockManager = MockESimProvisioningManager()
+        var state = makeTestState()
+        state.isSystemConfiguratorAvailable = false
+        
+        let store = TestStore(
+            initialState: state,
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onDeviceSettingTap) {
+            $0.destination = .eSimPhoneSettings
+        }
+    }
+    
+    // MARK: - onAppDidBecomeActive Tests
+    
+    func testOnAppDidBecomeActive_WithNewSim_ShowsActivationSuccess() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockNumberOfSims = 2
+        
+        var state = makeTestState()
+        state.currentNumberOfInstalledSims = 1
+        
+        let store = TestStore(
+            initialState: state,
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onAppDidBecomeActive) {
+            $0.destination = .activationSuccess
+        }
+    }
+    
+    func testOnAppDidBecomeActive_WithoutNewSim_DoesNothing() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockNumberOfSims = 1
+        
+        var state = makeTestState()
+        state.currentNumberOfInstalledSims = 1
+        
+        let store = TestStore(
+            initialState: state,
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onAppDidBecomeActive)
+    }
+    
+    func testOnAppDidBecomeActive_WithFewerSims_DoesNothing() async {
+        let mockManager = MockESimProvisioningManager()
+        mockManager.mockNumberOfSims = 0
+        
+        var state = makeTestState()
+        state.currentNumberOfInstalledSims = 1
+        
+        let store = TestStore(
+            initialState: state,
+            reducer: { ESimActivationReducer(eSimManager: mockManager) }
+        )
+        
+        await store.send(.onAppDidBecomeActive)
     }
 }
 
+// MARK: - Mock ESimProvisioningManager
 
-
-import CoreTelephony
-import UIKit
-
-protocol ESimProvisioningManaging: Sendable {
-    @MainActor func isESimSupportedByDevice() -> Bool
-    @MainActor func isSystemESimConfiguratorAvailable(configuratorLink: String) -> Bool
-    func numberOfAvailableSims() -> Int
-}
-
-final class ESimProvisioningManager: ESimProvisioningManaging {
-    func numberOfAvailableSims() -> Int {
-        let info = CTTelephonyNetworkInfo()
-        let numberOfProviders = info.serviceCurrentRadioAccessTechnology?.keys.count ?? 0
-        return numberOfProviders
+final class MockESimProvisioningManager: ESimProvisioningManaging {
+    var mockIsESimSupported = true
+    var mockIsConfiguratorAvailable = true
+    var mockNumberOfSims = 0
+    
+    @MainActor func isESimSupportedByDevice() -> Bool {
+        return mockIsESimSupported
     }
     
     @MainActor func isSystemESimConfiguratorAvailable(configuratorLink: String) -> Bool {
-        guard let url = URL(string: configuratorLink) else { return false }
-        return UIApplication.shared.canOpenURL(url)
+        return mockIsConfiguratorAvailable
     }
-
-    @MainActor func isESimSupportedByDevice() -> Bool {
-        isESimOnlyDevice() || isDualSimDevice() || containsMobileDataSettings()
+    
+    func numberOfAvailableSims() -> Int {
+        return mockNumberOfSims
     }
-
-    @MainActor private func containsMobileDataSettings() -> Bool {
-        if let url = URL(string: "App-Prefs:root=MOBILE_DATA_SETTINGS_ID") {
-            return UIApplication.shared.canOpenURL(url)
-        } else {
-            return false
-        }
-    }
-
-    private func isESimOnlyDevice() -> Bool {
-        let services = CTTelephonyNetworkInfo().dataServiceIdentifier
-
-        if services == nil || services?.isEmpty == true {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    private func isDualSimDevice() -> Bool {
-        if let radioAccesses = CTTelephonyNetworkInfo().serviceCurrentRadioAccessTechnology {
-            return radioAccesses.count > 1
-        } else {
-            return false
-        }
-    }
-}
-
-
-
-struct ESimActivationData: Equatable, Sendable {
-    let lpa: String
-    let smdpAddress: String
-    let activationCode: String
-    let iosActivationUrl: String
-    let confirmationCode: String
-    let carrierName: String
-    let planLabel: String
-    let cardNumber: String
 }
