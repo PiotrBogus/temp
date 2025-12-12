9683eaf5d272d1581a11bc2aa1b96367d08ce262
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-12-12 14:10:05 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,25 +1,16 @@
 InstallDetails_ActivationCode_btn_Copy: "0000.0000.0000.0000"
-
 InstallDetails_ActivationCodeLDAP_btn_Copy: "0000.0000.0000.0000"
-
 InstallDetails_AdressSMDP_btn_Copy: "0000.0000.0000.0000"
-
 InstallDetails_btn_PhoneSettings: "0000.0000.0000.0000"
-
 InstallDetails_view_Show: "0000.0000.0000.0000"
+
+InstallError_General_btn_OpenHome: "0000.0000.0000.0000"
+InstallError_General_view_OpenMyProducts: "0000.0000.0000.0000"
+InstallError_General_view_Show: "0000.0000.0000.0000"
+InstallError_UnsupportedDevice_btn_OK: "0000.0000.0000.0000"
+InstallError_UnsupportedDevice_view_Show: "0000.0000.0000.0000"
 
 InstallSplash_view_Show: "0000.0000.0000.0000"
 
-InstallError_UnsupportedDevice_view_Show: "0000.0000.0000.0000"
-
-InstallError_UnsupportedDevice_btn_OK: "0000.0000.0000.0000"
-
-InstallError_General_view_Show: "0000.0000.0000.0000"
-
-InstallError_General_btn_OpenHome: "0000.0000.0000.0000"
-
-InstallError_General_view_OpenMyProducts: "0000.0000.0000.0000"
-
-InstallSukcess_view_Show: "0000.0000.0000.0000"
-
 InstallSukcess_btn_OK: "0000.0000.0000.0000"
+InstallSukcess_view_Show: "0000.0000.0000.0000"
