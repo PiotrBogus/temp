c190bb676f8b0c1b4eca1e4e60024bd77a2d77fd
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2025-12-15 09:02:03 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,16 +1,143 @@
-InstallDetails_ActivationCode_btn_Copy: "0000.0000.0000.0000"
-InstallDetails_ActivationCodeLDAP_btn_Copy: "0000.0000.0000.0000"
-InstallDetails_AdressSMDP_btn_Copy: "0000.0000.0000.0000"
-InstallDetails_btn_PhoneSettings: "0000.0000.0000.0000"
-InstallDetails_view_Show: "0000.0000.0000.0000"
+InstallDetails_btn_Copy:
+  en: "Skopiuj"
+  pl: "Skopiuj"
+  ru: "Skopiuj"
+  uk: "Skopiuj"
 
-InstallError_General_btn_OpenHome: "0000.0000.0000.0000"
-InstallError_General_view_OpenMyProducts: "0000.0000.0000.0000"
-InstallError_General_view_Show: "0000.0000.0000.0000"
-InstallError_UnsupportedDevice_btn_OK: "0000.0000.0000.0000"
-InstallError_UnsupportedDevice_view_Show: "0000.0000.0000.0000"
+InstallDetails_lbl_ActivationCode:
+  en: "Kod aktywacyjny"
+  pl: "Kod aktywacyjny"
+  ru: "Kod aktywacyjny"
+  uk: "Kod aktywacyjny"
 
-InstallSplash_view_Show: "0000.0000.0000.0000"
+InstallDetails_lbl_ActivationCodeCopyMessage:
+  en: "The activation code was copied"
+  pl: "Kod aktywacyjny został skopiowany"
+  ru: "Kod aktywacyjny został skopiowany"
+  uk: "Kod aktywacyjny został skopiowany"
 
-InstallSukcess_btn_OK: "0000.0000.0000.0000"
-InstallSukcess_view_Show: "0000.0000.0000.0000"
+InstallDetails_lbl_ActivationCodeLPA:
+  en: "Kod aktywacyjny (LPA)"
+  pl: "Kod aktywacyjny (LPA)"
+  ru: "Kod aktywacyjny (LPA)"
+  uk: "Kod aktywacyjny (LPA)"
+
+InstallDetails_lbl_AddressSMDP:
+  en: "Adres SM-DP+"
+  pl: "Adres SM-DP+"
+  ru: "Adres SM-DP+"
+  uk: "Adres SM-DP+"
+
+InstallDetails_lbl_ContentMessage:
+  en: "Kartę eSIM możesz zainstalować w ustawieniach swojego urządzenia. Skorzystaj z instrukcji, którą przesłaliśmy na Twój adres e-mail."
+  pl: "Kartę eSIM możesz zainstalować w ustawieniach swojego urządzenia. Skorzystaj z instrukcji, którą przesłaliśmy na Twój adres e-mail."
+  ru: "Kartę eSIM możesz zainstalować w ustawieniach swojego urządzenia. Skorzystaj z instrukcji, którą przesłaliśmy na Twój adres e-mail."
+  uk: "Kartę eSIM możesz zainstalować w ustawieniach swojego urządzenia. Skorzystaj z instrukcji, którą przesłaliśmy na Twój adres e-mail."
+
+InstallDetails_lbl_SimNumber:
+  en: "Numer kart eSIM"
+  pl: "Numer kart eSIM"
+  ru: "Numer kart eSIM"
+  uk: "Numer kart eSIM"
+
+InstallDetails_lbl_SMDPCodeCopyMessage:
+  en: "Adress SM-DP+ code was copied"
+  pl: "Adres SM-DP+ został skopiowany"
+  ru: "Adres SM-DP+ został skopiowany"
+  uk: "Adres SM-DP+ został skopiowany"
+
+InstallDetails_lbl_TariffName:
+  en: "Nazwa usługi"
+  pl: "Nazwa usługi"
+  ru: "Nazwa usługi"
+  uk: "Nazwa usługi"
+
+InstallDetails_lbl_Title:
+  en: "Instalacja karty eSIM"
+  pl: "Instalacja karty eSIM"
+  ru: "Instalacja karty eSIM"
+  uk: "Instalacja karty eSIM"
+
+InstallError_btn_UnsupportedDeviceOk:
+  en: "OK"
+  pl: "OK"
+  ru: "OK"
+  uk: "OK"
+
+InstallError_lbl_GeneralDescription:
+  en: "Spróbuj ponownie później."
+  pl: "Spróbuj ponownie później."
+  ru: "Spróbuj ponownie później."
+  uk: "Spróbuj ponownie później."
+
+InstallError_lbl_GeneralTitle:
+  en: "Nie możesz teraz zainstalować karty eSIM"
+  pl: "Nie możesz teraz zainstalować karty eSIM"
+  ru: "Nie możesz teraz zainstalować karty eSIM"
+  uk: "Nie możesz teraz zainstalować karty eSIM"
+
+InstallError_lbl_UnsupportedDeviceDescription:
+  en: "Jeśli chcesz zainstalować kartę eSIM, skorzystaj z innego urządzenia."
+  pl: "Jeśli chcesz zainstalować kartę eSIM, skorzystaj z innego urządzenia."
+  ru: "Jeśli chcesz zainstalować kartę eSIM, skorzystaj z innego urządzenia."
+  uk: "Jeśli chcesz zainstalować kartę eSIM, skorzystaj z innego urządzenia."
+
+InstallError_lbl_UnsupportedDeviceTitle:
+  en: "Twój telefon nie obsługuje kart eSIM"
+  pl: "Twój telefon nie obsługuje kart eSIM"
+  ru: "Twój telefon nie obsługuje kart eSIM"
+  uk: "Twój telefon nie obsługuje kart eSIM"
+
+InstallSplash_lbl_Description:
+  en: "Nie zamykaj ekranu"
+  pl: "Nie zamykaj ekranu"
+  ru: "Nie zamykaj ekranu"
+  uk: "Nie zamykaj ekranu"
+
+InstallSplash_lbl_PageTitle:
+  en: "Instalacja karty eSIM"
+  pl: "Instalacja karty eSIM"
+  ru: "Instalacja karty eSIM"
+  uk: "Instalacja karty eSIM"
+
+InstallSplash_lbl_Title:
+  en: "Sprawdzamy ustawienia Twojego urządzenia"
+  pl: "Sprawdzamy ustawienia Twojego urządzenia"
+  ru: "Sprawdzamy ustawienia Twojego urządzenia"
+  uk: "Sprawdzamy ustawienia Twojego urządzenia"
+
+InstallSuccess_btn_OK:
+  en: "OK"
+  pl: "OK"
+  ru: "OK"
+  uk: "OK"
+
+InstallSuccess_lbl_Description:
+  en: "Karta aktywuje się, gdy połączysz się z lokalną siecią w kraju, w którym aktualnie jesteś. Wcześniej nie będzie działać."
+  pl: "Karta aktywuje się, gdy połączysz się z lokalną siecią w kraju, w którym aktualnie jesteś. Wcześniej nie będzie działać."
+  ru: "Karta aktywuje się, gdy połączysz się z lokalną siecią w kraju, w którym aktualnie jesteś. Wcześniej nie będzie działać."
+  uk: "Karta aktywuje się, gdy połączysz się z lokalną siecią w kraju, w którym aktualnie jesteś. Wcześniej nie będzie działać."
+
+InstallSuccess_lbl_Title:
+  en: "Karta eSIM została zainstalowana na tym urządzeniu"
+  pl: "Karta eSIM została zainstalowana na tym urządzeniu"
+  ru: "Karta eSIM została zainstalowana na tym urządzeniu"
+  uk: "Karta eSIM została zainstalowana na tym urządzeniu"
+
+Install_btn_DeviceSettings:
+  en: "Przejdź do ustawień urządzenia"
+  pl: "Przejdź do ustawień urządzenia"
+  ru: "Przejdź do ustawień urządzenia"
+  uk: "Przejdź do ustawień urządzenia"
+
+Install_btn_OpenHome:
+  en: "Przejdź do ekranu startowego"
+  pl: "Przejdź do ekranu startowego"
+  ru: "Przejdź do ekranu startowego"
+  uk: "Przejdź do ekranu startowego"
+
+Install_btn_OpenMyProducts:
+  en: "Przejdź do ekranu aktywnych Usług i zakupów"
+  pl: "Przejdź do ekranu aktywnych Usług i zakupów"
+  ru: "Przejdź do ekranu aktywnych Usług i zakupów"
+  uk: "Przejdź do ekranu aktywnych Usług i zakupów"
