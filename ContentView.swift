14686db4d33195e5b464f5fcec1b27dca67c0f52
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-19 22:27:49 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,169 +1,40 @@
-SWIFT TASK CONTINUATION MISUSE: stopSession() leaked its continuation without resuming it. This may cause tasks waiting on it to remain suspended forever.
-
-
-import BehavioralBiometric
-import Foundation
-@preconcurrency import IKOCommon
-import SwinjectAutoregistration
-import UIPinInterface
-
-final class BehavioralBiometricNetworkServiceProvider: BehavioralBiometricNetworkServiceProviding, @unchecked Sendable {
-    private let service: Service
-    private let core: IKOCore
-    private var partnerSessionId: String?
-
-    init(core: IKOCore = IKOAssembler.resolver~>) {
-        self.core = core
-        service = Service(with: CoreNetworking(core: core))
-    }
-
-    func changeBehavioralBiometricStatus(
-        isEnabled: Bool,
-        mPin: IKOUIPin,
-        agreements: [BehavioralBiometricAgreement]?
-    ) async throws {
-        try await withCheckedThrowingContinuation { [weak self] continuation in
-            guard let castedPin = mPin as? IKOCorePin else {
-                fatalError("Expecting IKOCorePin class")
-            }
-            let params = IKOCoreChangeBehavioralBiometricStateParams()
-            params.enabled = isEnabled
-            params.mPin = castedPin
-            params.agreements = agreements?.compactMap {
-                let agreement = IKOCoreBehavioralBiometricApiAgreement()
-                agreement.consentType = $0.consentType
-                agreement.textConsentId = $0.textConsentId
-                agreement.consent = true
-                return agreement
-            }
-
-            let operation = self?.core.requestChangeBehavioralBiometricStateWithParams(params) {
-                continuation.resume()
-            }
-            self?.handleError(for: operation, with: continuation)
-            self?.service.makeRequest(with: operation)
-        }
-    }
-
-    func getBehavioralBiometricAgreements() async throws -> [BehavioralBiometricAgreement] {
-        try await withCheckedThrowingContinuation { [weak self] continuation in
-            let operation = self?.core.requestGetBehavioralBiometricAgreementsWithCompletion { response in
-                guard let response else {
-                    continuation.resume(throwing: BehavioralBiometricError.emptyResponse)
-                    return
-                }
-                let agreements = response.agreements.compactMap {
-                    BehavioralBiometricAgreement(
-                        textConsentId: $0.textConsentId,
-                        consentType: $0.consentType,
-                        textShort: $0.textShort,
-                        textFull: $0.textFull,
-                        isMandatory: $0.mandatory
-                    )
-                }
-                continuation.resume(returning: agreements)
-            }
-            self?.handleError(for: operation, with: continuation)
-            self?.service.makeRequest(with: operation)
-        }
-    }
-
-    func getBehavioralBiometricState() async throws -> BehavioralBiometricState {
-        try await withCheckedThrowingContinuation { [weak self] continuation in
-            let operation = self?.core.requestGetBehavioralBiometricStateWithCompletion { response in
-                guard let response else {
-                    continuation.resume(throwing: BehavioralBiometricError.emptyResponse)
-                    return
-                }
-                continuation.resume(returning: BehavioralBiometricState(
-                    enabled: response.enabled,
-                    agreementsDate: response.agreementsDate,
-                    agreements: response.agreements.compactMap {
-                        BehavioralBiometricAgreement(
-                            textConsentId: $0.textConsentId,
-                            consentType: $0.consentType,
-                            textShort: $0.textShort,
-                            textFull: $0.textFull,
-                            isMandatory: $0.mandatory
-                        )
-                    }
-                ))
-            }
-            self?.handleError(for: operation, with: continuation)
-            self?.service.makeRequest(with: operation)
-        }
-    }
-
-    func startSession(latitude: String?, longitude: String?) async throws -> BehavioralBiometricStartSession {
-        guard partnerSessionId == nil else {
-            throw BehavioralBiometricError.sessionInProgress
-        }
-        return try await withCheckedThrowingContinuation { [weak self] continuation in
-            let params = IKOCoreStartBehavioralBiometricSessionParams()
-            params.latitude = latitude
-            params.longitude = longitude
-
-            let operation = self?.core.requestStartBehavioralBiometricSessionWithParams(params) { response in
-                guard let response else {
-                    continuation.resume(throwing: BehavioralBiometricError.emptyResponse)
-                    return
-                }
-                self?.partnerSessionId = response.partnerSessionId
-                continuation.resume(returning: BehavioralBiometricStartSession(
-                    enabled: response.enabled,
-                    cssId: response.cssId
-                ))
-            }
-            self?.handleError(for: operation, with: continuation)
-            self?.service.makeRequest(with: operation)
-        }
-    }
-
-    func stopSession() async throws {
-        guard let partnerSessionId else {
-            throw BehavioralBiometricError.missingPartnerSessionId
-        }
-        try await withCheckedThrowingContinuation { [weak self] continuation in
-            let params = IKOCoreStopBehavioralBiometricSessionParams()
-            params.partnerSessionId = partnerSessionId
-            self?.partnerSessionId = nil
-            let operation = self?.core.requestStopBehavioralBiometricSessionWithParams(params) {
-                continuation.resume()
-            }
-            self?.handleError(for: operation, with: continuation)
-            self?.service.makeRequest(with: operation)
-        }
-    }
-
-    private func handleError<T>(
-        for operation: IKOCoreOperation?,
-        with continuation: CheckedContinuation<T, Error>
-    ) {
-        operation?.errorHandler = { coreError in
-            continuation.resume(
-                throwing: BehavioralBiometricError.general(
-                    title: coreError?.errorTitle,
-                    description: coreError?.errorDescription
-                )
-            )
-        }
-
-        operation?.failHandler = { _ in
-            continuation.resume(throwing: BehavioralBiometricError.general(title: nil, description: nil))
-            return true
-        }
-
-        operation?.resultHandler.timeoutHandler = {
-            continuation.resume(throwing: BehavioralBiometricError.timeout)
-        }
-
-        operation?.fieldErrorsHandler = { coreError in
-            continuation.resume(
-                throwing: BehavioralBiometricError.fieldErrors(
-                    title: coreError?.errorTitle,
-                    description: coreError?.errorDescription
-                )
-            )
-        }
-    }
-}
+9480a7fae HEAD@{24}: rebase (continue): feat(card_transactions): IKOFKC-203239 Bazowa implementacja ekranu Usług walutowych
+b55548ab78 HEAD@{25}: rebase (continue): feat(card_transactions): IKOFKC-203238 Utworzenie modułu DebitCardCurrencyServices
+2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{26}: rebase (start): checkout rc-3.80
+cb0c29b274 HEAD@{27}: checkout: moving from rc-3.80 to change-1059230
+2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{28}: checkout: moving from change-1059230 to rc-3.80
+cb0c29b274 HEAD@{29}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+3aa39d7d12 HEAD@{30}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+e4414e4599 HEAD@{31}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria beh:...skipping...
+36334e0630 (HEAD -> change-1059230) HEAD@{0}: checkout: moving from rc-3.80 to change-1059230
+f40e486c04 (rc-3.80) HEAD@{1}: commit: feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{2}: checkout: moving from change-1059230 to rc-3.80
+36334e0630 (HEAD -> change-1059230) HEAD@{3}: rebase (finish): returning to refs/heads/change-1059230
+36334e0630 (HEAD -> change-1059230) HEAD@{4}: rebase (continue): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+c3ed6cee3a HEAD@{5}: rebase (pick): feat(acc_details): IKOFKC-213879 Usunięcie możliwości pobrania potwierdzenia (pdf) dla blokady w historii konta
+16154217ee HEAD@{6}: rebase (pick): feat(partner_abc): IKOFKC-213818 Aktualizacja KeyVisual na Landing Page usługi Allegro Klik
+a9255b2763 HEAD@{7}: rebase (continue): fix(card_transactions): IKOFKC-213455 Blokada DCC - Możliwość przełączania nieaktywnego switcha
+09e7245645 HEAD@{8}: rebase (pick): fix(push): IKOFKC-212390 Nieprawidłowa obsługa PUSH podczas przenoszenia MA między urządzeniami
+b110a1ae2a HEAD@{9}: rebase (pick): refactor(exchange): IKOFKC-213678 ExchangeRatesReducer refactoring. Poprawa prezentowania generic error
+c28d0d692a HEAD@{10}: rebase (pick): feat(card_details): IKOFKC-212734 Zmiana warunku prezentacji punktu wejścia
+904436e14a HEAD@{11}: rebase (pick): refactor(card_details): IKOFKC-210384 Migracja do nowego modułu Labels
+78b1245ef3 HEAD@{12}: rebase (pick): feat(card_transactions): IKOFKC-210379 Prezentacja wizerunku karty przy użyciu KeyValue
+af9373b700 HEAD@{13}: rebase (pick): fix(card_details): IKOFKC-210174 Brak punktu wejścia w Usługi walutowe
+c9e003e60f HEAD@{14}: rebase (pick): fix(card_transactions): IKOFKC-210171 Ekran błędu gdy nie da się pobrać wizerunku karty
+61944cd00c HEAD@{15}: rebase (pick): feat(card_transactions): IKOFKC-203229 Testy jednostkowe ekranu Usług Walutowych
+a18f81baa1 HEAD@{16}: rebase (continue): feat(card_transactions): IKOFKC-209998 Usunięcie starych ekranów Usług Walutowych
+33327e0deb HEAD@{17}: rebase (continue): feat(card_transactions): IKOFKC-210121 Obsługa przekierowania na ekran Usług Walutowych
+8c13a82499 HEAD@{18}: rebase (continue): feat(card_transactions): IKOFKC-209997 Dynamiczne przenoszenie kwoty i waluty do nowej linii
+a8a14ef107 HEAD@{19}: rebase (pick): feat(card_transactions): IKOFKC-203230 Podpięcie nowego ekranu do punktu wejścia
+281ec2d628 HEAD@{20}: rebase (continue): feat(card_transactions): IKOFKC-209850 Zapisywanie wprowadzonych zmian w Usługach walutowych
+a28d12e9dc HEAD@{21}: rebase (continue): feat(card_transactions): IKOFKC-202983 Obsługa ekranów Empty State dla Usług walutowych
+d50ef3aa55 HEAD@{22}: rebase (pick): feat(card_transactions): IKOFKC-202980 Sekcja kont do podpięcia na ekranie Usług walutowych
+61e5bf2bc5 HEAD@{23}: rebase (pick): feat(card_transactions): IKOFKC-203240 Dodanie sekcji "Blokada transakcji DCC"
+09480a7fae HEAD@{24}: rebase (continue): feat(card_transactions): IKOFKC-203239 Bazowa implementacja ekranu Usług walutowych
+b55548ab78 HEAD@{25}: rebase (continue): feat(card_transactions): IKOFKC-203238 Utworzenie modułu DebitCardCurrencyServices
+2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{26}: rebase (start): checkout rc-3.80
+cb0c29b274 HEAD@{27}: checkout: moving from rc-3.80 to change-1059230
+2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{28}: checkout: moving from change-1059230 to rc-3.80
+cb0c29b274 HEAD@{29}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+3aa39d7d12 HEAD@{30}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+e4414e4599 HEAD@{31}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
