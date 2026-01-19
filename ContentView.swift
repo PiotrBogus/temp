0be6ad93d94eedc664235b6fcb19b2a3f56c4b76
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-19 22:30:39 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,40 +1,276 @@
-9480a7fae HEAD@{24}: rebase (continue): feat(card_transactions): IKOFKC-203239 Bazowa implementacja ekranu Usług walutowych
-b55548ab78 HEAD@{25}: rebase (continue): feat(card_transactions): IKOFKC-203238 Utworzenie modułu DebitCardCurrencyServices
-2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{26}: rebase (start): checkout rc-3.80
-cb0c29b274 HEAD@{27}: checkout: moving from rc-3.80 to change-1059230
-2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{28}: checkout: moving from change-1059230 to rc-3.80
-cb0c29b274 HEAD@{29}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-3aa39d7d12 HEAD@{30}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-e4414e4599 HEAD@{31}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria beh:...skipping...
-36334e0630 (HEAD -> change-1059230) HEAD@{0}: checkout: moving from rc-3.80 to change-1059230
-f40e486c04 (rc-3.80) HEAD@{1}: commit: feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{2}: checkout: moving from change-1059230 to rc-3.80
-36334e0630 (HEAD -> change-1059230) HEAD@{3}: rebase (finish): returning to refs/heads/change-1059230
-36334e0630 (HEAD -> change-1059230) HEAD@{4}: rebase (continue): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-c3ed6cee3a HEAD@{5}: rebase (pick): feat(acc_details): IKOFKC-213879 Usunięcie możliwości pobrania potwierdzenia (pdf) dla blokady w historii konta
-16154217ee HEAD@{6}: rebase (pick): feat(partner_abc): IKOFKC-213818 Aktualizacja KeyVisual na Landing Page usługi Allegro Klik
-a9255b2763 HEAD@{7}: rebase (continue): fix(card_transactions): IKOFKC-213455 Blokada DCC - Możliwość przełączania nieaktywnego switcha
-09e7245645 HEAD@{8}: rebase (pick): fix(push): IKOFKC-212390 Nieprawidłowa obsługa PUSH podczas przenoszenia MA między urządzeniami
-b110a1ae2a HEAD@{9}: rebase (pick): refactor(exchange): IKOFKC-213678 ExchangeRatesReducer refactoring. Poprawa prezentowania generic error
-c28d0d692a HEAD@{10}: rebase (pick): feat(card_details): IKOFKC-212734 Zmiana warunku prezentacji punktu wejścia
-904436e14a HEAD@{11}: rebase (pick): refactor(card_details): IKOFKC-210384 Migracja do nowego modułu Labels
-78b1245ef3 HEAD@{12}: rebase (pick): feat(card_transactions): IKOFKC-210379 Prezentacja wizerunku karty przy użyciu KeyValue
-af9373b700 HEAD@{13}: rebase (pick): fix(card_details): IKOFKC-210174 Brak punktu wejścia w Usługi walutowe
-c9e003e60f HEAD@{14}: rebase (pick): fix(card_transactions): IKOFKC-210171 Ekran błędu gdy nie da się pobrać wizerunku karty
-61944cd00c HEAD@{15}: rebase (pick): feat(card_transactions): IKOFKC-203229 Testy jednostkowe ekranu Usług Walutowych
-a18f81baa1 HEAD@{16}: rebase (continue): feat(card_transactions): IKOFKC-209998 Usunięcie starych ekranów Usług Walutowych
-33327e0deb HEAD@{17}: rebase (continue): feat(card_transactions): IKOFKC-210121 Obsługa przekierowania na ekran Usług Walutowych
-8c13a82499 HEAD@{18}: rebase (continue): feat(card_transactions): IKOFKC-209997 Dynamiczne przenoszenie kwoty i waluty do nowej linii
-a8a14ef107 HEAD@{19}: rebase (pick): feat(card_transactions): IKOFKC-203230 Podpięcie nowego ekranu do punktu wejścia
-281ec2d628 HEAD@{20}: rebase (continue): feat(card_transactions): IKOFKC-209850 Zapisywanie wprowadzonych zmian w Usługach walutowych
-a28d12e9dc HEAD@{21}: rebase (continue): feat(card_transactions): IKOFKC-202983 Obsługa ekranów Empty State dla Usług walutowych
-d50ef3aa55 HEAD@{22}: rebase (pick): feat(card_transactions): IKOFKC-202980 Sekcja kont do podpięcia na ekranie Usług walutowych
-61e5bf2bc5 HEAD@{23}: rebase (pick): feat(card_transactions): IKOFKC-203240 Dodanie sekcji "Blokada transakcji DCC"
-09480a7fae HEAD@{24}: rebase (continue): feat(card_transactions): IKOFKC-203239 Bazowa implementacja ekranu Usług walutowych
-b55548ab78 HEAD@{25}: rebase (continue): feat(card_transactions): IKOFKC-203238 Utworzenie modułu DebitCardCurrencyServices
-2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{26}: rebase (start): checkout rc-3.80
-cb0c29b274 HEAD@{27}: checkout: moving from rc-3.80 to change-1059230
-2ffa2de0ac (tag: v-3-180-1, tag: mob_iko3-ios-3-180-1, origin/rc-3.80, origin/feature/visaoffers) HEAD@{28}: checkout: moving from change-1059230 to rc-3.80
-cb0c29b274 HEAD@{29}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-3aa39d7d12 HEAD@{30}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
-e4414e4599 HEAD@{31}: commit (amend): feat(bio_beh): IKOFKC-173966 Biometria behawioralna
+	
+Your turn (25 and more)
+		Subject	Owner	Reviewers	Repo	Branch	Waiting	Size	Status	CR	NACR	V
+
+	
+refactor(arch): IKOFKC-199777 Wyniesienie folderu Defines z IKOCommon do osobnego modułu
+ 
+Wysocki Kamil (EXT)
+Bogus
+, 
+Cackowski
+, +19
+mob-iko3-ios	feature/ikocommon-refactoring-part-4	6 days	XL	 Ready	
+
+	
+feat(pkoid): IKOFKC-213737 Przygotowanie ekranu informacyjnego w metodach EID
+ 
+Cackowski Przemysław (EXT)
+Bogus
+, 
+Czapnik
+, +19
+mob-iko3-ios	feature/pkoid-eid	6 days	XL	 2 missing 	
+
+	
+feat(avatars): IKOFKC-216101 Optymilizacja lifecycle ContextPickera na dashboardzie
+ 
+Hanzel Szymon (EXT)
+Bogus
+, 
+Cackowski
+, +19
+mob-iko3-ios	feature/avatars	4 days	M	 2 missing 	
+-1
+
+	
+feat(avatars): IKOFKC-213271 OfferBar zachęcający do otwarcia konta
+ 
+Rytel Hubert (EXT)
+Bogus
+, 
+Cackowski
+, +19
+mob-iko3-ios	feature/avatars	3 days	M	 3 missing 	
+-1
+-1
+
+	
+feat(avatars): IKOFKC-213270 Zapisywanie informacji o wyświetleniu oferty
+ 
+Rytel Hubert (EXT)
+Bogus
+, 
+Cackowski
+, +19
+mob-iko3-ios	feature/avatars	3 days	M	 Ready	
+
+	
+feat(card_transactions): IKOFKC-203238 Utworzenie modułu DebitCardCurrencyServices
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 44 min	L	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-203240 Dodanie sekcji "Blokada transakcji DCC"
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 44 min	M	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-202983 Obsługa ekranów Empty State dla Usług walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 44 min	XL	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-202980 Sekcja kont do podpięcia na ekranie Usług walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 44 min	M	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-209850 Zapisywanie wprowadzonych zmian w Usługach walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 44 min	L	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-203239 Bazowa implementacja ekranu Usług walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 43 min	L	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-203230 Podpięcie nowego ekranu do punktu wejścia
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 41 min	L	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-209997 Dynamiczne przenoszenie kwoty i waluty do nowej linii
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 40 min	M	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-209998 Usunięcie starych ekranów Usług Walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 40 min	L	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-210121 Obsługa przekierowania na ekran Usług Walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 39 min	M	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-203229 Testy jednostkowe ekranu Usług Walutowych
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 39 min	M	 3 missing	
+-1
+
+	
+fix(card_transactions): IKOFKC-210171 Ekran błędu gdy nie da się pobrać wizerunku karty
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 39 min	S	 3 missing	
+-1
+
+	
+fix(card_details): IKOFKC-210174 Brak punktu wejścia w Usługi walutowe
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 38 min	M	 3 missing	
+-1
+
+	
+feat(card_transactions): IKOFKC-210379 Prezentacja wizerunku karty przy użyciu KeyValue
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 37 min	M	 3 missing	
+-1
+
+	
+feat(card_details): IKOFKC-212734 Zmiana warunku prezentacji punktu wejścia
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 37 min	XS	 3 missing	
+-1
+
+	
+refactor(card_details): IKOFKC-210384 Migracja do nowego modułu Labels
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 36 min	M	 3 missing	
+-1
+
+	
+refactor(exchange): IKOFKC-213678 ExchangeRatesReducer refactoring. Poprawa prezentowania generic error
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	1 hour 35 min	M	 3 missing	
+-1
+
+	
+fix(card_transactions): IKOFKC-213455 Blokada DCC - Możliwość przełączania nieaktywnego switcha
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	58 minutes	S	 3 missing	
+-1
+
+	
+feat(acc_details): IKOFKC-213879 Usunięcie możliwości pobrania potwierdzenia (pdf) dla blokady w historii konta
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	57 minutes	S	 3 missing	
+-1
+
+	
+feat(partner_abc): IKOFKC-213818 Aktualizacja KeyVisual na Landing Page usługi Allegro Klik
+ 
+Bogus Piotr (EXT)
+Cackowski
+, 
+Czapnik
+, +18
+mob-iko3-ios	rc-3.80	56 minutes	S	 3 missing	
+-1
