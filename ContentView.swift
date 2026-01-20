7856edfe2f1c474541e37b1c719f659635d77ab5
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-20 11:53:53 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,2 +1,24 @@
- ! [remote rejected]       HEAD -> refs/for/rc-3.80 (no new changes)
-error: failed to push some refs to 'ssh://gerrit.pkobp.pl:29418/mob-iko3-ios'
+Executed 44 tests, with 0 failures (0 unexpected) in 0.788 (0.801) seconds
+2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 214.713 elapsed -- Testing started completed.
+2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 0.000 sec, +0.000 sec -- start
+2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 214.713 sec, +214.713 sec -- end
+** TEST FAILED **
+
+==> Deleting simulator devices...
+132B6BDD-3BA8-4B3F-8CC5-CEC624C3C901
+==> "Run tests" finished in 0:14:43
+==> "Continuous Integration" finished in 0:17:28
++ exit 65
+Build step 'Execute shell' marked build as failure
+Archiving artifacts
+[Checks API] No suitable checks publisher found.
+Notifying upstream projects of job completion
+Email was triggered for: Failure - Any
+Sending email for trigger: Failure - Any
+Not sending mail to unregistered user piotr.bogus@pkobp.pl because your SCM claimed this was associated with a user ID ‘piotr.bogus' which your security realm does not recognize; you may need changes in your SCM plugin
+Sending email to: karol.czapnik.2@pkobp.pl
+[WS-CLEANUP] Deleting project workspace...
+[WS-CLEANUP] Deferred wipeout is used...
+[WS-CLEANUP] done
+Triggering a new build of gerrit-verification
+Finished: FAILURE
