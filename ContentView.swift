cfa54142d9d1388d438436b4d0aa222f6e76946c
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-20 14:41:04 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,24 +1,49 @@
-Executed 44 tests, with 0 failures (0 unexpected) in 0.788 (0.801) seconds
-2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 214.713 elapsed -- Testing started completed.
-2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 0.000 sec, +0.000 sec -- start
-2026-01-20 11:48:00.113 xcodebuild[40771:94744294] [MT] IDETestOperationsObserverDebug: 214.713 sec, +214.713 sec -- end
-** TEST FAILED **
+import Assembly
+import Dependencies
+import DependenciesMacros
+import SwinjectAutoregistration
+/// Performs all logout related operations (e.g. clearing caches, context, etc).
+@DependencyClient
+public struct LogoutOperationsPerformer: Sendable {
+    /// Perform all operations.
+    public var perform: @Sendable () -> Void
+}
 
-==> Deleting simulator devices...
-132B6BDD-3BA8-4B3F-8CC5-CEC624C3C901
-==> "Run tests" finished in 0:14:43
-==> "Continuous Integration" finished in 0:17:28
-+ exit 65
-Build step 'Execute shell' marked build as failure
-Archiving artifacts
-[Checks API] No suitable checks publisher found.
-Notifying upstream projects of job completion
-Email was triggered for: Failure - Any
-Sending email for trigger: Failure - Any
-Not sending mail to unregistered user piotr.bogus@pkobp.pl because your SCM claimed this was associated with a user ID ‘piotr.bogus' which your security realm does not recognize; you may need changes in your SCM plugin
-Sending email to: karol.czapnik.2@pkobp.pl
-[WS-CLEANUP] Deleting project workspace...
-[WS-CLEANUP] Deferred wipeout is used...
-[WS-CLEANUP] done
-Triggering a new build of gerrit-verification
-Finished: FAILURE
+extension LogoutOperationsPerformer: TestDependencyKey {
+    public static let testValue = LogoutOperationsPerformer(perform: { })
+}
+
+extension LogoutOperationsPerformer: DependencyKey {
+    public static let liveValue = LogoutOperationsPerformer {
+        let behavioralBiometricClearer: BehavioralBiometricLogoutClearing = IKOAssembler.resolver~>
+        @Dependency(AnalyticsStore.self) var analyticsStore
+        @Dependency(BlikResetService.self) var blikResetService
+        @Dependency(OpenBankingResetService.self) var openBankingResetService
+        @Dependency(MobileAuthorizationResetService.self) var mobileAuthorizationResetService
+        @Dependency(ContextClearer.self) var contextClearer
+        @Dependency(ClosedAdvertsOfferDataClearer.self) var closedAdvertsOfferDataClearer
+        @Dependency(OffersSessionDataClearer.self) var offersSessionDataClearer
+        @Dependency(UserContextFlusher.self) var userContextFlusher
+        @Dependency(Talk2IKOConfigurationResetService.self) var talk2IKOConfigurationResetService
+        @Dependency(NetworkRequestCanceller.self) var networkRequestCanceller
+        @Dependency(SessionLoginDateClearer.self) var sessionLoginDateClearer
+        @Dependency(GreetingsLogoutCounter.self) var greetingLogoutCounter
+        @Dependency(EnablersLoader.self) var enablersLoader
+
+        behavioralBiometricClearer.clear()
+        analyticsStore.storeAnalyticsEvents()
+        blikResetService.reset()
+        openBankingResetService.reset()
+        mobileAuthorizationResetService.reset()
+        contextClearer.clear()
+        closedAdvertsOfferDataClearer.clear()
+        offersSessionDataClearer.clear()
+        userContextFlusher.flush()
+        talk2IKOConfigurationResetService.reset()
+        networkRequestCanceller.cancelAll()
+        sessionLoginDateClearer.clear()
+        greetingLogoutCounter.increase()
+        analyticsStore.clearStoredSessionIdentifier()
+        enablersLoader.loadEnablersForDefaultContext()
+    }
+}
