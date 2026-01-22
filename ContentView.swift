1f372cda872aaab6e037c92cb69276d12416c0a3
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-22 20:54:41 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,1 +1,8 @@
+    public func hideError(for index: Int) {
         guard checkboxes.endIndex < index else { return }
+        let checkbox = checkboxes[index].checkbox
+        checkbox.hideError()
+        if errorContainer.isHidden, checkboxesWithErrorCount == .zero {
+            state = .normal
+        }
+    }
