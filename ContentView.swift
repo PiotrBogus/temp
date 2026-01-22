d8fa839e3a8258718407d36412b3c47a47e1a0ec
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-22 20:54:20 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,1 @@
-                return .merge(
-                    .send {
-                        .chartContainer(
-                            .set(
-                                selectors: shouldUpdateSelectors ? aggregate.selectors : nil,
-                                dimensions: aggregate.dimensions
-                            )
-                        )
-                    },
-                    .send {
-                        .delegate(.setTitle(aggregate.title))
-                    }
-                )
+        guard checkboxes.endIndex < index else { return }
