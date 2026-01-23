83e1c97739c5663a56489be6eb24d60e5a1e52d5
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-23 17:53:29 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,26 +1,12 @@
     private var questionsAndAnswersSection: some View {
         VStack(alignment: .leading, spacing: .zero) {
-            Separator()
-            HStack {
-                HeaderView(text: .labels.BehavioralBiometric_InfoScreen_lbl_BottomSectionHeader.localized)
-                    .with(topSeparatorHidden: true)
-                    .with(bottomSeparatorHidden: true)
-                Spacer()
-                Button {
-                    store.send(.onShowAllQuestionsAndAnswersButtonTap)
-                } label: {
-                    AttributedText(.labels.BehavioralBiometric_InfoScreen_btn_BottomSectionAll.localized)
-                        .fontStyle(Constants.showAllQuestionsAndAnswersButtonFontStyle)
-                }
-                .padding(.trailing, Constants.commonPadding)
-            }
             WithViewStore(store, observe: { $0 }) { viewStore in
-                ForEach(store.questionsAndAnswersItems) { item in
+                ForEach(viewStore.questionsAndAnswersItems) { item in
                     Collapse(
                         isExpanded: viewStore.binding(
                             get: { $0.expandedId == item.id },
                             send: { isExpanded in
-                                store.send(.onQuestionTap(isExpanded ? item.id : nil))
+                                .onQuestionTap(isExpanded ? item.id : nil)
                             }
                         )
                     ) {
@@ -37,7 +23,6 @@
         }
         .background(Color(uiColor: .ikoWhite100))
     }
-
 
 
 The compiler is unable to type-check this expression in reasonable time; try breaking up the expression into distinct sub-expressions
