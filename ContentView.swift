ec27670904142e90815927a7d4a4080c03bccb6d
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-28 15:28:06 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,138 +1,270 @@
-@testable import Authorization
-import Dependencies
-import Testing
+import ComposableArchitecture
+import GenieCommonDomain
+import SwiftUI
 
-@Suite("Logout operations performer tests")
-struct LogoutOperationsPerformerTests {
-    let sut = LogoutOperationsPerformer.liveValue
+struct WatchlistEditView: View {
+    @Bindable var store: StoreOf<WatchlistFeature>
 
-    @Test("When invoking perform, it should do all necessary operations")
-    // swiftlint:disable:next function_body_length
-    func perform() {
-        let storeAnalyticsEventsInvokedCount = LockIsolated(0)
-        let blikResetInvokedCount = LockIsolated(0)
-        let openBankingResetInvokedCount = LockIsolated(0)
-        let mobileAuthorizationResetInvokedCount = LockIsolated(0)
-        let closedAdvertsOfferDataInvokedCount = LockIsolated(0)
-        let offersSessionDataClearInvokedCount = LockIsolated(0)
-        let clearContextInvokedCount = LockIsolated(0)
-        let flushUserContextInvokedCount = LockIsolated(0)
-        let talk2IKOConfigurationResetInvokedCount = LockIsolated(0)
-        let cancelAllRequestsInvokedCount = LockIsolated(0)
-        let sessionLoginDateClearInvokedCount = LockIsolated(0)
-        let increaseLogoutCounterInvokedCount = LockIsolated(0)
-        let clearStoredSessionIdentifierInvokedCount = LockIsolated(0)
-        let loadEnablersForDefaultContextInvokedCount = LockIsolated(0)
+    var body: some View {
+        WatchlistTableView(store: store)
+    }
+}
 
-        withDependencies {
-            $0[AnalyticsStore.self].storeAnalyticsEvents = {
-                storeAnalyticsEventsInvokedCount.withValue { $0 += 1 }
+struct WatchlistTableView: View {
+    @Bindable var store: StoreOf<WatchlistFeature>
+
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).ignoresSafeArea()
+            VStack(spacing: 0) {
+                List {
+                    ForEach(store.sectionedSelectedItems, id: \.section) { sectionGroup in
+                        Section(
+                            header: SectionHeaderView(sectionName: sectionGroup.section)
+                        ) {
+                            ForEach(sectionGroup.items, id: \.id) { item in
+                                cellView(item: item)
+                                    .listRowInsets(EdgeInsets())
+                                    .listRowBackground(Color(.systemBackground))
+                                    .listRowSeparator(
+                                        item == sectionGroup.items.last ? .hidden : .visible)
+                            }
+                            .onDelete { offsets in
+                                store.send(
+                                    .deleteItems(offsets, section: sectionGroup.section))
+                            }
+                            .onMove { source, destination in
+                                store.send(
+                                    .moveItems(
+                                        source: source, destination: destination,
+                                        section: sectionGroup.section))
+                            }
+                        }
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    }
+                }
+                .listStyle(.plain)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .environment(\.editMode, $store.editMode.sending(\.setEditMode))
             }
-            $0[BlikResetService.self].reset = {
-                blikResetInvokedCount.withValue { $0 += 1 }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Watchlist")
+                        .font(.title2)
+                        .foregroundStyle(Color.accentColor)
+                }
+
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        store.send(.delegate(.pushToAdd))
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
             }
-            $0[OpenBankingResetService.self].reset = {
-                openBankingResetInvokedCount.withValue { $0 += 1 }
+            .tint(Color.accentColor)
+            .overlay {
+                if store.selectedItems.isEmpty {
+                    EmptyItemsView(hasSearchText: false)
+                }
             }
-            $0[MobileAuthorizationResetService.self].reset = {
-                mobileAuthorizationResetInvokedCount.withValue { $0 += 1 }
+        }
+    }
+
+    @ViewBuilder
+    private func cellView(item: WatchlistConfigurationItemEntity) -> some View {
+        HStack {
+            Text(item.title)
+                .font(.body)
+                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+            Spacer()
+        }
+        .padding(.vertical, 2)
+    }
+
+}
+
+#Preview("Watchlist Manager") {
+    NavigationView {
+        WatchlistEditView(
+            store: Store(initialState: WatchlistFeature.State()) {
+                WatchlistFeature()
+            })
+    }
+}
+
+
+
+import ComposableArchitecture
+import GenieCommonDomain
+import SwiftUI
+
+@Reducer
+public struct WatchlistFeature {
+    public init() {}
+
+    @ObservableState
+    public struct State: Equatable, Sendable {
+        public var allItems: [WatchlistConfigurationItemEntity] = []
+        public var selectedWatchlistItemsIds: [Int] = []
+        public var searchText: String = ""
+        public var editMode: EditMode = .active
+
+        public init() {}
+
+        public var availableItems: [WatchlistConfigurationItemEntity] {
+            let filtered = allItems.filter { !selectedWatchlistItemsIds.contains($0.id) }
+
+            if searchText.isEmpty {
+                return filtered
+            } else {
+                return filtered.filter {
+                    $0.name.localizedCaseInsensitiveContains(searchText)
+                    || $0.section.localizedCaseInsensitiveContains(searchText)
+                }
             }
-            $0[ContextClearer.self].clear = {
-                clearContextInvokedCount.withValue { $0 += 1 }
-            }
-            $0[ClosedAdvertsOfferDataClearer.self].clear = {
-                closedAdvertsOfferDataInvokedCount.withValue { $0 += 1 }
-            }
-            $0[OffersSessionDataClearer.self].clear = {
-                offersSessionDataClearInvokedCount.withValue { $0 += 1 }
-            }
-            $0[UserContextFlusher.self].flush = {
-                flushUserContextInvokedCount.withValue { $0 += 1 }
-            }
-            $0[Talk2IKOConfigurationResetService.self].reset = {
-                talk2IKOConfigurationResetInvokedCount.withValue { $0 += 1 }
-            }
-            $0[NetworkRequestCanceller.self].cancelAll = {
-                cancelAllRequestsInvokedCount.withValue { $0 += 1 }
-            }
-            $0[SessionLoginDateClearer.self].clear = {
-                sessionLoginDateClearInvokedCount.withValue { $0 += 1 }
-            }
-            $0[GreetingsLogoutCounter.self].increase = {
-                increaseLogoutCounterInvokedCount.withValue { $0 += 1 }
-            }
-            $0[AnalyticsStore.self].clearStoredSessionIdentifier = {
-                clearStoredSessionIdentifierInvokedCount.withValue { $0 += 1 }
-            }
-            $0[EnablersLoader.self].loadEnablersForDefaultContext = {
-                loadEnablersForDefaultContextInvokedCount.withValue { $0 += 1 }
-            }
-        } operation: {
-            await sut.perform()
         }
 
-        #expect(storeAnalyticsEventsInvokedCount.value == 1, "It should store all analytics events")
-        #expect(blikResetInvokedCount.value == 1, "It should reset BLIK Manager")
-        #expect(openBankingResetInvokedCount.value == 1, "It should reset Open Banking status tracker")
-        #expect(mobileAuthorizationResetInvokedCount.value == 1, "It should reset Mobile Authorization transaction manager")
-        #expect(clearContextInvokedCount.value == 1, "It should clear current context (MSP, Individual or child)")
-        #expect(closedAdvertsOfferDataInvokedCount.value == 1, "It should clear adverts session data")
-        #expect(offersSessionDataClearInvokedCount.value == 1, "It should clear offers session data")
-        #expect(flushUserContextInvokedCount.value == 1, "It should flush current user context")
-        #expect(talk2IKOConfigurationResetInvokedCount.value == 1, "It should reset Talk 2 IKO configuration")
-        #expect(cancelAllRequestsInvokedCount.value == 1, "It should cancel all requests")
-        #expect(sessionLoginDateClearInvokedCount.value == 1, "It should clear last login date")
-        #expect(increaseLogoutCounterInvokedCount.value == 1, "It should increase logout counter")
-        #expect(clearStoredSessionIdentifierInvokedCount.value == 1, "It should clear stored session identifier")
-        #expect(loadEnablersForDefaultContextInvokedCount.value == 1, "It should load enablers for default context")
+        public var sectionedSelectedItems: [SectionGroup] {
+            sectioned(selectedItems)
+        }
+
+        public var sectionedAllItems: [SectionGroup] {
+            sectioned(allItems)
+        }
+    }
+
+    public enum Action: Sendable, Equatable {
+        case onAppear
+        case dataLoaded(WatchlistConfigurationEntity)
+        case addItem(WatchlistConfigurationItemEntity)
+        case clearSearch
+        case deleteItems(IndexSet, section: String)
+        case delegate(Delegate)
+        case moveItems(source: IndexSet, destination: Int, section: String)
+        case removeItem(WatchlistConfigurationItemEntity)
+        case searchTextChanged(String)
+        case setEditMode(EditMode)
+
+        public enum Delegate: Sendable {
+            case pushToAdd
+        }
+    }
+
+    @Dependency(\.watchlistFeatureClient) var client
+
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return fetchData()
+
+            case .dataLoaded(let configuration):
+                state.allItems = configuration.watchlistItems
+                state.selectedWatchlistItemsIds = configuration.selectedWatchlistItemsIds
+                return .none
+
+            case let .addItem(item):
+                if !state.selectedItems.contains(where: { $0.id == item.id }) {
+                    state.selectedItems.append(item)
+                }
+                return .none
+
+            case let .removeItem(item):
+                state.selectedItems.removeAll(where: { $0.id == item.id })
+                return .none
+
+            case let .deleteItems(offsets, section):
+                let itemsToDelete = state.selectedItems
+                    .enumerated()
+                    .compactMap { index, item in
+                        item.section == section ? index : nil
+                    }
+
+                let indicesToDelete = itemsToDelete.enumerated().compactMap {
+                    localIndex, globalIndex in
+                    offsets.contains(localIndex) ? globalIndex : nil
+                }
+
+                state.selectedItems.remove(atOffsets: IndexSet(indicesToDelete))
+                return .none
+
+            case let .moveItems(source, destination, section):
+                let sectionItems = state.selectedItems.filter { $0.section == section }
+                guard !sectionItems.isEmpty else { return .none }
+
+                guard
+                    let globalStartIndex = state.selectedItems.firstIndex(where: {
+                        $0.section == section
+                    })
+                else {
+                    return .none
+                }
+
+                let globalSourceIndices = IndexSet(source.map { globalStartIndex + $0 })
+                let globalDestination = globalStartIndex + destination
+
+                state.selectedItems.move(
+                    fromOffsets: globalSourceIndices, toOffset: globalDestination)
+                return .none
+
+            case let .searchTextChanged(text):
+                state.searchText = text
+                return .none
+
+            case .clearSearch:
+                state.searchText = ""
+                return .none
+
+            case let .setEditMode(editMode):
+                state.editMode = editMode
+                return .none
+
+            case .delegate:
+                return .none
+            }
+        }
     }
 }
 
+extension WatchlistFeature {
+    private func fetchData() -> Effect<Action> {
 
-import Dependencies
-import DependenciesMacros
-/// Performs all logout related operations (e.g. clearing caches, context, etc).
-@DependencyClient
-public struct LogoutOperationsPerformer: Sendable {
-    /// Perform all operations.
-    public var perform: @Sendable () async -> Void
-}
-
-extension LogoutOperationsPerformer: TestDependencyKey {
-    public static let testValue = LogoutOperationsPerformer(perform: { })
-}
-
-extension LogoutOperationsPerformer: DependencyKey {
-    public static let liveValue = LogoutOperationsPerformer {
-        @Dependency(\.behavioralBiometricClearer) var behavioralBiometricClearer
-        @Dependency(AnalyticsStore.self) var analyticsStore
-        @Dependency(BlikResetService.self) var blikResetService
-        @Dependency(OpenBankingResetService.self) var openBankingResetService
-        @Dependency(MobileAuthorizationResetService.self) var mobileAuthorizationResetService
-        @Dependency(ContextClearer.self) var contextClearer
-        @Dependency(ClosedAdvertsOfferDataClearer.self) var closedAdvertsOfferDataClearer
-        @Dependency(OffersSessionDataClearer.self) var offersSessionDataClearer
-        @Dependency(UserContextFlusher.self) var userContextFlusher
-        @Dependency(Talk2IKOConfigurationResetService.self) var talk2IKOConfigurationResetService
-        @Dependency(NetworkRequestCanceller.self) var networkRequestCanceller
-        @Dependency(SessionLoginDateClearer.self) var sessionLoginDateClearer
-        @Dependency(GreetingsLogoutCounter.self) var greetingLogoutCounter
-        @Dependency(EnablersLoader.self) var enablersLoader
-
-        await behavioralBiometricClearer.clear()
-        analyticsStore.storeAnalyticsEvents()
-        blikResetService.reset()
-        openBankingResetService.reset()
-        mobileAuthorizationResetService.reset()
-        contextClearer.clear()
-        closedAdvertsOfferDataClearer.clear()
-        offersSessionDataClearer.clear()
-        userContextFlusher.flush()
-        talk2IKOConfigurationResetService.reset()
-        networkRequestCanceller.cancelAll()
-        sessionLoginDateClearer.clear()
-        greetingLogoutCounter.increase()
-        analyticsStore.clearStoredSessionIdentifier()
-        enablersLoader.loadEnablersForDefaultContext()
+        return .run { send in
+            let watchlistConfiguration = try await client.fetchConfigurationData()
+            await send(.dataLoaded(watchlistConfiguration))
+        } catch: { error, _ in
+            print(error)
+        }
     }
+}
+
+// MARK: - Data Models
+
+extension WatchlistFeature {
+    public struct SectionGroup: Equatable {
+        public let section: String
+        public let items: [WatchlistConfigurationItemEntity]
+
+        public init(section: String, items: [WatchlistConfigurationItemEntity]) {
+            self.section = section
+            self.items = items
+        }
+    }
+}
+
+// MARK: - Helper Functions
+
+private func sectioned(_ items: [WatchlistConfigurationItemEntity]) -> [WatchlistFeature.SectionGroup] {
+    let sections = items.filter { $0.parentId != nil }
+    let itemsWithoutSections = items.filter { $0.parentId == nil }
+
+    let sections = sections.compactMap { section in
+        WatchlistFeature.SectionGroup(
+            section: section.title,
+            items: itemsWithoutSections.filter { section.id != $0.id }
+        )
+    }
+    return sections
 }
