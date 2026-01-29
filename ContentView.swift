89dba230bfb1d07b433a9e24ed826473f228c586
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-01-29 22:08:14 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,3 +1,22 @@
-Expected declaration
-    var updateData: @Sendable (_ visibleItems: [Int]) async throws
-                                                            ^
+    private func getSectionHeaderStyle(section: WatchlistFeature.SectionGroup) -> SectionHeaderView.Style {
+
+        if section.items.allSatisfy { item in
+            store.selectedItems.contains { $0.id == item.id }
+        } {
+            return SectionHeaderView.fullySelectedItems
+        }
+
+        var isEmpty = true
+        if store.selectedItems.forEach { item in
+            if section.items.contains { $0.id == item.id } {
+                isEmpty = false
+                break
+            }
+        }
+
+        if isEmpty {
+            return SectionHeaderView.empty
+        } else {
+            return SectionHeaderView.partialySelectedItems
+        }
+    }
