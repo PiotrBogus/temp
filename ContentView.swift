a1ab15a23f506080cd8f8c32ba9aa26dad904959
PiotrBogus <36243625+PiotrBogus@users.noreply.github.com>
2026-02-10 16:50:00 +0100

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,1 +1,22 @@
-Cannot convert value of type 'any KeyPath<AlternativeItemFeature.State, IdentifiedArrayOf<AlternativeItemFeature.State>> & Sendable' (aka 'any KeyPath<AlternativeItemFeature.State, IdentifiedArray<Int, AlternativeItemFeature.State>> & Sendable') to expected argument type 'WritableKeyPath<AlternativeItemFeature.State, IdentifiedArray<Int, AlternativeItemFeature.State>>'
+private func resetTreeFromAllItems(
+    _ allItems: IdentifiedArrayOf<AlternativeItemFeature.State>,
+    isDisabled: Bool
+) -> IdentifiedArrayOf<AlternativeItemFeature.State> {
+
+    func reset(_ items: IdentifiedArrayOf<AlternativeItemFeature.State>)
+    -> IdentifiedArrayOf<AlternativeItemFeature.State> {
+
+        IdentifiedArray(
+            uniqueElements: items.map { item in
+                var item = item
+                item.isExpanded = false
+                item.isSelected = false
+                item.isDisabled = isDisabled
+                item.children = reset(item.children)
+                return item
+            }
+        )
+    }
+
+    return reset(allItems)
+}
